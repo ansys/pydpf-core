35a9caf64b2c1e2e6a794e08be18de8562c59046
lparadis <lea.paradis@ansys.com>
2020-11-19 17:15:52 +0100

    run_client.bat updated

--- run_client.bat
+++ run_client.bat
@@ -1,13 +1,19 @@
 
 @echo on
 
-set ANS_PROTOCOL_ROOT=E:\AnsysDev\Protocols
-set PYTHONPATH=%ANS_PROTOCOL_ROOT%\packages\python\dpf
+set ANSYS_PATH=C:\Program Files\ANSYS Inc\v212\
+set DPF_CONFIGURATION=release
+set DPF_PATH=C:\Program Files\ANSYS Inc\v212\aisol\bin\winx64\Ans.Dpf.Grpc.exe
+set CODEDV_ROOT=D:\AnsysDev\CodeDV
+set ANS_PROTOCOL_ROOT=D:\AnsysDev\Protocols
+
+set OPERATORS_DATABASE_PATH = %CODEDV_ROOT%\DataProcessing\Ans.Dpf.Grpc\Python\
+set PYTHONPATH=%ANS_PROTOCOL_ROOT%\packages\python\dpf;%CODEDV_ROOT%\DataProcessing\Ans.Dpf.Grpc\Python;%OPERATORS_DATABASE_PATH%
 
 set root=C:\ProgramData\Anaconda3
 call %root%\Scripts\activate.bat %root%
 
-call jupyter notebook
+call jupyter lab
 
  
 
