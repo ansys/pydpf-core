6751270bc75a3a00b2eb96e10ea56761af781cde
Alexander Kaszynski <alexander.kaszynski@ansys.com>
2020-12-08 13:38:44 -0700

    refactored test_operator.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,7 @@ pv.OFF_SCREEN = True
 running_docker = os.environ.get('DPF_DOCKER', False)
 
 
-def resolve_test_file(basename):
+def resolve_test_file(basename, additional_path=''):
     """Resolves a test file's full path based on the base name and the enviornment.
 
     Normally returns local path unless server is running on docker and
@@ -28,12 +28,12 @@ def resolve_test_file(basename):
     if running_docker:
         # assumes repository root is mounted at '/dpf'
         test_files_path = '/dpf/tests/testfiles'
-        return os.path.join(test_files_path, basename)
+        return os.path.join(test_files_path, additional_path, basename)
     else:
         # otherwise, assume file is local
         test_path = os.path.dirname(os.path.abspath(__file__))
         test_files_path = os.path.join(test_path, 'testfiles')
-        filename = os.path.join(test_files_path, basename)
+        filename = os.path.join(test_files_path, additional_path, basename)
         if not os.path.isfile(filename):
             raise FileNotFoundError(f'Unable to locate {basename} at {test_files_path}')
         return filename
@@ -41,17 +41,42 @@ def resolve_test_file(basename):
 
 @pytest.fixture()
 def allkindofcomplexity():
-    """Path of the "allKindOfComplexity.rst" result file."""
+    """Resolve the path of the "allKindOfComplexity.rst" result file."""
     return resolve_test_file('allKindOfComplexity.rst')
 
 
 @pytest.fixture()
 def simple_bar():
-    """Path of the "ASimpleBar.rst" result file."""
+    """Resolve the path of the "ASimpleBar.rst" result file."""
     return resolve_test_file('ASimpleBar.rst')
+
+
+@pytest.fixture()
+def velocity_acceleration():
+    """Resolve the path of the "velocity_acceleration.rst" result file."""
+    return resolve_test_file('velocity_acceleration.rst')
+
+
+@pytest.fixture()
+def cyclic_lin_rst():
+    """Resolve the path of the "cyclic/file.rst" result file."""
+    return resolve_test_file('file.rst', 'cyclic')
+
+
+@pytest.fixture()
+def cyclic_ds():
+    """Resolve the path of the "cyclic/ds.dat" result file."""
+    return resolve_test_file('ds.dat', 'cyclic')
+
+
+@pytest.fixture()
+def fields_container_csv():
+    """Resolve the path of the "csvToField/fields_container.csv" result file."""
+    return resolve_test_file('fields_container.csv', 'csvToField')
 
 
 @pytest.fixture(scope="session", autouse=True)
 def load_operators(request):
     """This loads all the operators on initialization"""
     model = core.Model(resolve_test_file('ASimpleBar.rst'))
+
