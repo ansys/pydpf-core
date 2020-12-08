47520fbfdfc2d6e87e7183585a681ff5f38b859b
lparadis <lea.paradis@ansys.com>
2020-12-08 17:39:37 +0100

    run_client.bat updated to v211

--- run_client.bat
+++ run_client.bat
@@ -1,9 +1,9 @@
 
 @echo on
 
-set ANSYS_PATH=C:\Program Files\ANSYS Inc\v212\
+set ANSYS_PATH=C:\Program Files\ANSYS Inc\v211\
 set DPF_CONFIGURATION=release
-set DPF_PATH=C:\Program Files\ANSYS Inc\v212\aisol\bin\winx64\Ans.Dpf.Grpc.exe
+set DPF_PATH=C:\Program Files\ANSYS Inc\v211\aisol\bin\winx64\Ans.Dpf.Grpc.exe
 set ANS_PROTOCOL_ROOT=D:\AnsysDev\Protocols
 set DPF_CORE_PATH=D:\AnsysDev\DPF-Core
 
