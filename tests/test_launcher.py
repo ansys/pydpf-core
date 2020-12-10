429387531473ddc3812dc9dfe75a24df77c16043
Alex Kaszynski <akascap@gmail.com>
2020-12-10 08:51:51 -0700

    skip entire launcher when ANSYS 2021R1 not found

--- tests/test_launcher.py
+++ tests/test_launcher.py
@@ -1,17 +1,23 @@
+import os
 import pytest
 
 from ansys.dpf import core
 
+
 ansys_path = core.misc.find_ansys()
+
+invalid_version = None
 if ansys_path is not None:
     try:
         invalid_version = int(ansys_path[-3:]) < 211
     except:
         invalid_version = True
 
+# skip unless ansys v211 is installed
+if ansys_path is None or invalid_version:
+    pytestmark = pytest.mark.skip("Requires local install of ANSYS 202R1")
 
-@pytest.mark.skipif(ansys_path is None or invalid_version,
-                    reason='Requires local install of ANSYS v211')
+
 def test_start_local():
     starting_channel = id(core.CHANNEL)
     n_init = len(core._server_instances)
