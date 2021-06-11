60ff303bf5f98fc7f7d87c50bdb79508d8015a00
cbellot <camille.bellot@ansys.com>
2021-06-11 13:53:00 +0200

    file path

--- ansys/dpf/core/examples/examples.py
+++ ansys/dpf/core/examples/examples.py
@@ -1,13 +1,12 @@
 import os
 import inspect
-import pathlib
 
 if os.environ.get('DPF_DOCKER', '').lower() == 'true':
     # must pass a path that can be accessed by a docker image with
     # this directory mounted at the repository level for CI
     _module_path = r'/dpf/ansys/dpf/core/examples/'
 else:
-    _module_path = pathlib.Path(__file__).parent.absolute()
+    _module_path = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
 # this files can be imported with from `ansys.dpf.core import examples`:
 simple_bar = os.path.join(_module_path, 'ASimpleBar.rst')
