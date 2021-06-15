94231b7c098606976499d3766fce6232c0437000
cbellot <camille.bellot@ansys.com>
2021-06-15 14:26:02 +0200

    doc

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -1,3 +1,4 @@
+ECHO %AWP_ROOT212%
 set SPHINX_APIDOC_OPTIONS=inherited-members
 call sphinx-apidoc -o ../docs/source/api ../ansys ../ansys/dpf/core/aeneid.py -f --implicit-namespaces --separate  --no-headings
 pushd .
