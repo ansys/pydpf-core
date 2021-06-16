dbf97d9da128efe75df7c6c7cc03f33928f5e546
Lea Paradis <lparadis@lyoc7mbudev1.ansys.com>
2021-06-16 15:01:16 +0200

    cff_plugin test added

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -6,16 +6,17 @@ import os
 
 def try_load_cff_operators():
     try:
-        if os.name=="posix":
-            return False
         dpf.load_library("Ans.Dpf.CFF.dll","cff")
         return True
     except:
-        return False
+        try:
+            dpf.load_library("libAns.Dpf.CFF.so", "cff")
+        except: 
+            return False
     #TODO: add loading for linux
         
 @pytest.mark.skipif(not try_load_cff_operators(), reason="Couldn't load cff operators")
-def test_cff(cff_data_sources):
+def test_cff_model(cff_data_sources):
     m = dpf.Model(cff_data_sources)
     assert m.metadata.meshed_region.nodes.n_nodes ==1430
     op = dpf.Operator("cff::cas::SV_DENSITY")
@@ -23,6 +24,31 @@ def test_cff(cff_data_sources):
     fc= op.get_output(0, dpf.types.fields_container)
     assert len(fc[0])==1380
 
+@pytest.mark.skipif(not try_load_cff_operators(), reason="Couldn't load cff operators")
+def test_cff_stream_operator(cff_data_sources):
+    stream_op = dpf.Operator("cff::stream_provider")
+    stream_op.connect(4, cff_data_sources)
+    res_info = dpf.Operator("ResultInfoProvider")
+    res_info.connect(3, stream_op, 0)
+    info = res_info.outputs.result_info()
+    assert info.analysis_type == 'unknown_analysis'
+    assert info.physics_type == 'unknown_physics'
+    assert info.n_results == 15
+    assert info.unit_system == "unknown"
+    res_info_2 = dpf.Operator("cff::cas::ResultInfoProvider")
+    res_info_2.connect(3, stream_op, 0)
+    info_2 = res_info_2.outputs.result_info()
+    assert info_2.analysis_type == 'unknown_analysis'
+    assert info_2.physics_type == 'unknown_physics'
+    assert info_2.n_results == 15
+    assert info_2.unit_system == "unknown"
+    op = dpf.Operator("cff::cas::SV_V")
+    op.connect(3, stream_op, 0)
+    field = op.get_output(0, dpf.types.fields_container)[0]
+    data = field.data
+    assert len(data) == 1450
+    assert field.location == dpf.locations.elemental
+    
 
 def try_load_lsdyna_operators():
     try:
