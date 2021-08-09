6f7550559af60ef103a578576b8615027ce87013
lparadis <lea.paradis@ansys.com>
2021-08-09 16:39:56 +0200

    Docstring moved from __init__ to class to allow sphinx to use it

--- ansys/dpf/core/element_descriptor.py
+++ ansys/dpf/core/element_descriptor.py
@@ -6,49 +6,42 @@ ElementDescriptor
 class ElementDescriptor: 
     """Class that describes an element.
     
+    Parameters
+    ----------
+    element_id: int
+    
+    description: str
+        Specifies the element geometry and integration order.
+    
+    name: str
+    
+    shape: str
+        Can be "solid", "shell" of "beam". 
+    
+    number_of_corner_nodes: int
+    
+    number_of_mid_nodes: int
+    
+    number_of_nodes: int
+        
+    is_solid: bool
+    
+    is_shell: bool
+    
+    is_beam: bool
+    
+    is_quadratic: bool
+    
     Examples
     --------
-    from ansys.dpf import core as dpf
-    element = dpf.ElementDescriptor(10, "Linear 4-nodes Tetrahedron", "tet4", "solid", 4, 0, 4, true, false, false, false)   
+    >>> from ansys.dpf import core as dpf
+    >>> descriptor = dpf.ElementDescriptor(10, "Linear 4-nodes Tetrahedron", "tet4", "solid", 4, 0, 4, True, False, False, False)   
     
     """
     
     def __init__(self, element_id, description, name, shape = None, number_of_corner_nodes = None, number_of_mid_nodes = None, 
                  number_of_nodes = None, is_solid = None, is_shell = None, is_beam = None, is_quadratic = None):
-        """Constructor of ElementDescriptor.
-        
-        Parameters
-        ----------
-        element_id: int
-        
-        description: str
-            Specifies the element geometry and integration order.
-        
-        name: str
-        
-        shape: str
-            Can be "solid", "shell" of "beam". 
-        
-        number_of_corner_nodes: int
-        
-        number_of_mid_nodes: int
-        
-        number_of_nodes: int
-            
-        is_solid: bool
-        
-        is_shell: bool
-        
-        is_beam: bool
-        
-        is_quadratic: bool
-        
-        Examples
-        --------
-        >>> from ansys.dpf import core as dpf
-        >>> descriptor = dpf.ElementDescriptor(10, "Linear 4-nodes Tetrahedron", "tet4", "solid", 4, 0, 4, True, False, False, False)   
-        
-        """
+        """Constructor of ElementDescriptor."""
         self.element_id = element_id
         self.description = description
         self.name = name
