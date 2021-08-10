2bc218aa2805e5141613fea4d5f07f5927a1f47d
lparadis <lea.paradis@ansys.com>
2021-08-10 09:04:59 +0200

    Add __ansys_consumer_version__ global variable

--- ansys/dpf/core/_version.py
+++ ansys/dpf/core/_version.py
@@ -5,4 +5,5 @@ version_info = 0, 3, 'dev0'
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
 __ansys_version__ = "212"
+__ansys_consumer_version__ = "2021 R2"
 min_server_version = "2.0"
