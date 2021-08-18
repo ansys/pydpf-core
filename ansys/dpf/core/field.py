0db9692894b31823b668042e873b8e9d2e8d1392
Alex Kaszynski <akascap@gmail.com>
2021-08-18 16:52:45 -0600

    Update ansys/dpf/core/field.py

--- ansys/dpf/core/field.py
+++ ansys/dpf/core/field.py
@@ -92,7 +92,7 @@ class Field(_FieldBase):
     >>> fields_container = disp.outputs.fields_container()
     >>> field = fields_container[0]
     >>> field.data[2]
-    array([-0.00672665, -0.03213735, 0.00016716])
+    array([-0.00672665, -0.03213735,  0.00016716])
     
     """
     
