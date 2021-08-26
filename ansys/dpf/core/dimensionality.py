cf1cd907fef0cf62edb6b346809bd8cf8ea1e4ad
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-08-26 11:55:22 -0400

    Update dimensionality.py

--- ansys/dpf/core/dimensionality.py
+++ ansys/dpf/core/dimensionality.py
@@ -15,7 +15,7 @@ class Dimensionality:
     ----------
     dim_vec : list
         List of integers for the dimension vectors. There is a 1D vector for a 
-        scalar or #D vector. There is a 2D vector for a matrix. For example:
+        scalar or 3D vector. There is a 2D vector for a matrix. For example:
         
         * ``[1]``: scalar
         * ``[3]``: 3d vector
