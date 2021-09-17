676cd5f722e4608ed095aa562adcb40912024ed7
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-09-17 16:06:00 -0400

    Update 01-transient_easy_time_scoping.py

--- examples/01-static-transient/01-transient_easy_time_scoping.py
+++ examples/01-static-transient/01-transient_easy_time_scoping.py
@@ -1,10 +1,10 @@
 """
 .. _ref_transient_easy_time_scoping:
 
-Go Easily over Times Transient Analysis
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-This example shows how to use model's results to easily
-choose time scoping
+Choose a Time Scoping for a Transient Analysis
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This example shows how to use a model's results to easily
+choose a time scoping.
 
 """
 import numpy as np
@@ -16,9 +16,8 @@ from ansys.dpf.core import operators as ops
 
 
 ###############################################################################
-# Create the model and display the state of the result.  Note
-# that this transient result file contains several individual results,
-# each at a different times.
+# Create the model and display the state of the result. This transient result
+# file contains several individual results, each at a different times.
 
 transient = examples.msup_transient
 model = dpf.Model(transient)
@@ -26,14 +25,14 @@ print(model)
 
 
 ###############################################################################
-# Obtain Minimum and Maximum Displacements at all times
+# Obtain Minimum and Maximum Displacements at All Times
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Create a displacement operator and set its time scoping request to
-# the entire time freq support.
+# the entire time frequency support:
 disp = model.results.displacement
 disp_op = disp.on_all_time_freqs()
 
-# Then chain the displacement operator with norm and min_max operators
+# Chain the displacement operator with norm and min_max operators.
 min_max_op =ops.min_max.min_max_fc(ops.math.norm_fc(disp_op))
 
 min_disp = min_max_op.outputs.field_min()
@@ -41,7 +40,7 @@ max_disp = min_max_op.outputs.field_max()
 print(max_disp.data)
 
 ###############################################################################
-# Plot the minimum and maximum displacements over time
+# Plot the minimum and maximum displacements over time:
 
 tdata = model.metadata.time_freq_support.time_frequencies.data
 plt.plot(tdata, max_disp.data, 'r', label='Max')
@@ -53,22 +52,22 @@ plt.show()
 
 
 ###############################################################################
-# Use time extrapolation
+# Use Time Extrapolation
 # ~~~~~~~~~~~~~~~~~~~~~~~
-# A local maximum can be seen on the plot between 0.05 and 0.075 s
+# A local maximum can be seen on the plot between 0.05 and 0.075 seconds.
 # Displacement will be evaluated every 0.0005s in this range
-# to draw a nicer plot on this range
+# to draw a nicer plot on this range.
 
 offset = 0.0005
 time_scoping = [0.05+offset*i for i in range(0, int((0.08-0.05)/offset))]
 print(time_scoping)
 
 ###############################################################################
-# Create a displacement operator and set its time scoping request
+# Create a displacement operator and set its time scoping request:
 disp = model.results.displacement
 disp_op = disp.on_time_scoping(time_scoping)()
 
-# Then chain the displacement operator with norm and min_max operators
+# Chain the displacement operator with norm and min_max operators.
 min_max_op =ops.min_max.min_max_fc(ops.math.norm_fc(disp_op))
 
 min_disp = min_max_op.outputs.field_min()
@@ -76,7 +75,7 @@ max_disp = min_max_op.outputs.field_max()
 print(max_disp.data)
 
 ###############################################################################
-# Plot the minimum and maximum displacements over time
+# Plot the minimum and maximum displacements over time:
 
 plt.plot(time_scoping, max_disp.data, 'rx', label='Max')
 plt.xlabel("Time (s)")
