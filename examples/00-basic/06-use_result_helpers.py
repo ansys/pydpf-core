07b9a188afc1eccd2a46b60c2ee45cecdfd692a6
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-09-20 16:27:09 -0400

    Update 06-use_result_helpers.py

--- examples/00-basic/06-use_result_helpers.py
+++ examples/00-basic/06-use_result_helpers.py
@@ -2,11 +2,13 @@
 .. _ref_use_result_helpers:
 
 Use Result Helpers to Load Custom Data
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-The `Result` class which instances are created by the `Model` gives access to 
-helpers to request results on specific mesh and time scopings.
-With those helpers, working on a custom spatial and temporal subset of the 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The ``Result`` class, which are instances created by the ``Model``, give
+access to helpers for requesting results on specific mesh and time scopings.
+With these helpers, working on a custom spatial and temporal subset of the 
 model is straightforward.
+
+Import necessary modules:
 """
 import numpy as np
 
@@ -15,33 +17,32 @@ from ansys.dpf.core import examples
 from ansys.dpf.core import operators as ops
 
 ###############################################################################
-# First, create a model object to establish a connection with an
-# example result file
+# Create a model object to establish a connection with an example result file:
 model = dpf.Model(examples.download_multi_stage_cyclic_result())
 print(model)
 
 
 ###############################################################################
-# Visualize specific mode shapes
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Choose the modes to visualize
+# Visualize Specific Mode Shapes
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Choose the modes to visualize:
 modes = [1,5,6]
 
 
 disp = model.results.displacement.on_time_scoping(modes)
 
 ###############################################################################
-# Choose a spatial subset
-# ~~~~~~~~~~~~~~~~~~~~~~~~~
-# Work only a named selection (or component)
+# Choose a Spatial Subset
+# ~~~~~~~~~~~~~~~~~~~~~~~
+# Work on only a named selection (or component).
 
 ###############################################################################
-# Print the available named selection
+# Print the available named selection:
 print(model.metadata.available_named_selections)
 
 
 ###############################################################################
-# Specify to the result that we work on a specific named selection
+# Specify to the result that you want to work on a specific named selection:
 disp.on_named_selection('_STAG1_BASE_NOD')
 op = disp()
 op.inputs.read_cyclic(2)#expand cyclic 
@@ -55,7 +56,7 @@ for mode in modes:
 
 
 ###############################################################################
-# Specify to the result that we work on specific nodes
+# Specify to the result that you want to work on specific nodes:
 disp = model.results.displacement.on_time_scoping(modes)
 disp.on_mesh_scoping(list(range(1, 200)))
 op = disp()
