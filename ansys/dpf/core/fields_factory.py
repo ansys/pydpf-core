55395d969482653140c1fc9cbeef0235f1d8f199
cbellot <camille.bellot@ansys.com>
2021-10-18 13:49:26 +0200

    fix

--- ansys/dpf/core/fields_factory.py
+++ ansys/dpf/core/fields_factory.py
@@ -120,12 +120,12 @@ def create_3d_vector_field(num_entities, location=locations.nodal, server=None):
         Number of entities to reserve
     location : str, optional
         Location of the field. The default is ``"Nodal"``. For example:
-            
+
         - ansys.dpf.core.natures.nodal (``"Nodal"``)
         - ansys.dpf.core.natures.elemental (``"Elemental"``)
         - ansys.dpf.core.natures.elemental_nodal (``"ElementalNodal"``)
         - ...
-    
+
 
     server : ansys.dpf.core.server, optional
         Server with the channel connected to the remote or local instance.
@@ -160,7 +160,7 @@ def create_tensor_field(num_entities, location=locations.nodal, server=None):
         Number of entities to reserve.
     location : str, optional
         Location of the field. The default is ``"Nodal"``. For example:
-        
+
         - :class:`ansys.dpf.core.natures.nodal` (``"Nodal"``)
         - :class:`ansys.dpf.core.natures.elemental` (``"Elemental"``)
         - :class:`ansys.dpf.core.natures.elemental_nodal` (``"ElementalNodal"``)
@@ -199,7 +199,7 @@ def create_scalar_field(num_entities, location=locations.nodal, server=None):
         Number of entities to reserve
     location : str, optional
         Location of the field. The default is ``"Nodal"``. For example:
-            
+
         - ansys.dpf.core.natures.nodal (``"Nodal"``)
         - ansys.dpf.core.natures.elemental (``"Elemental"``)
         - ansys.dpf.core.natures.elemental_nodal (``"ElementalNodal"``)
@@ -240,7 +240,7 @@ def create_vector_field(num_entities, num_comp, location=locations.nodal, server
         Number of vector components.
     location : str, optional
         Location of the field. The default is ``"Nodal"``. For example:
-    
+
         - ansys.dpf.core.natures.nodal (``"Nodal"``)
         - ansys.dpf.core.natures.elemental (``"Elemental"``)
         - ansys.dpf.core.natures.elemental_nodal (``"ElementalNodal"``)
@@ -302,12 +302,12 @@ def _create_field(
         Number of entities to reserve.
     location : str optional
         Location of the field. For example:
-        
+
         - :class:`ansys.dpf.core.natures.nodal` (``"Nodal"``)
         - :class:`ansys.dpf.core.natures.elemental` (``"Elemental"``)
         - :class:`ansys.dpf.core.natures.elemental_nodal` (``"ElementalNodal"``)
         - ...
-        - ...   
+        - ...
 
     ncomp_n : int
         Number of lines.
