eff47c21eaa45bb69951f220cdebc76fcc0bd6af
Kathy Pippert <84872299+PipKat@users.noreply.github.com>
2021-10-21 15:17:01 -0400

    Update 06-use_result_helpers.py

--- examples/00-basic/06-use_result_helpers.py
+++ examples/00-basic/06-use_result_helpers.py
@@ -3,7 +3,7 @@
 
 Use Result Helpers to Load Custom Data
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-The ``Result`` class, which are instances created by the ``Model``, give
+The ``Result`` class, which is an instance created by the ``Model``, gives
 access to helpers for requesting results on specific mesh and time scopings.
 With these helpers, working on a custom spatial and temporal subset of the
 model is straightforward.
