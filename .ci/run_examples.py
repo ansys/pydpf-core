2838b2d485e09c7e552d2b55313737c4f6a61696
cbellot <camille.bellot@ansys.com>
2021-10-28 10:22:32 +0200

    module

--- .ci/run_examples.py
+++ .ci/run_examples.py
@@ -1,7 +1,20 @@
 import os
 import glob
 import pathlib
+from ansys.dpf.core.misc import module_exists
 import subprocess
+
+if module_exists("matplotlib"):
+    import matplotlib as mpl
+
+    mpl.use("Agg")
+
+# enable off_screen plotting to avoid test interruption
+
+if module_exists("pyvista"):
+    import pyvista as pv
+
+    pv.OFF_SCREEN = True
 
 actual_path = pathlib.Path(__file__).parent.absolute()
 print(os.path.join(actual_path,os.path.pardir, "examples"))
@@ -12,6 +25,8 @@ for root, subdirectories, files in os.walk(os.path.join(actual_path, os.path.par
             print("\n\n--------------------------------------------------\n")
             print(file)
             print("--------------------------------------------------\n")
-            p = subprocess.check_output(["python", file],
-                                 stderr=subprocess.STDOUT,
-                                 shell=False)
+            exec(
+                open(file, mode="r", encoding="utf8").read(),
+                globals(),
+                globals(),
+            )
