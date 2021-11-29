f608955fe89bb578af66c60ff2659d27860df4d4
jleonatti <84793532+jleonatti@users.noreply.github.com>
2021-11-29 08:13:08 -0500

    Add linux doc build (#122)
    * Add linux doc build
    
    * Add sphinx-apidoc

--- examples/04-specific-requests/00-hdf5_double_float_comparison.py
+++ examples/04-specific-requests/00-hdf5_double_float_comparison.py
@@ -56,20 +56,24 @@ h5op.inputs.data3.connect(mesh)
 ###############################################################################
 # Export with simple precision
 
-h5op.inputs.file_path.connect(os.path.join(tmpdir, "c:/temp/dpf_float.h5"))
+directory = "c:/temp/"
+if os.name == "posix":
+    directory = "/tmp/"
+
+h5op.inputs.file_path.connect(os.path.join(tmpdir, directory, "dpf_float.h5"))
 h5op.run()
 
 ###############################################################################
 # Export with simple precision
 
 h5op.inputs.export_floats.connect(False)
-h5op.inputs.file_path.connect(os.path.join(tmpdir, "c:/temp/dpf_double.h5"))
+h5op.inputs.file_path.connect(os.path.join(tmpdir, directory, "dpf_double.h5"))
 h5op.run()
 
 ###############################################################################
 # Comparison
-float_precision = os.stat(os.path.join(tmpdir, "c:/temp/dpf_float.h5")).st_size
-double_precision = os.stat(os.path.join(tmpdir, "c:/temp/dpf_double.h5")).st_size
+float_precision = os.stat(os.path.join(tmpdir, directory, "dpf_float.h5")).st_size
+double_precision = os.stat(os.path.join(tmpdir, directory, "dpf_double.h5")).st_size
 print(
     f"size with float precision: {float_precision}\n"
     f"size with double precision: {double_precision}"
