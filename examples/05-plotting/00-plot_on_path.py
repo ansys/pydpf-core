ed7152ee5f56f6cbda76fdae78ae7dbf1597bd08
lparadis <lea.paradis@ansys.com>
2021-12-08 15:22:27 +0100

    Refactore examples - plotting section added

---
+++ examples/05-plotting/00-plot_on_path.py
@@ -0,0 +1,62 @@
+"""
+.. _plot_on_path:
+
+Plot results on a specific path
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This example shows how to get a result mapped over a specific path,
+and how to plot it. 
+
+"""
+
+from ansys.dpf import core as dpf
+from ansys.dpf.core import examples
+
+###############################################################################
+# Path plotting
+# ~~~~~~~~~~~~~
+# We will use a DpfPlotter to plot a mapped result over a defined path of
+# coordinates.
+
+# First, we need to create the model, request its mesh and its 
+# displacement data
+model = dpf.Model(examples.multishells_rst)
+mesh = model.metadata.meshed_region
+disp_fc = model.results.displacement().outputs.fields_container()
+field = disp_fc[0]
+
+# Then, we create a coordinates field to map on
+i = 0
+coordinates = []
+ref = [-0.0155, 0.00600634, -0.0025]
+coordinates.append(ref)
+while i < 20:
+    i += 1
+    coord_copy = ref.copy()
+    coord_copy[0] = coord_copy[0] + i * 0.0003
+    coordinates.append(coord_copy)
+field_coord = dpf.fields_factory.create_3d_vector_field(len(coordinates))
+field_coord.data = coordinates
+field_coord.scoping.ids = list(range(1, len(coordinates) + 1))
+
+# Let's now compute the mapped data using the mapping operator
+mapping_operator = dpf.Operator("mapping")
+mapping_operator.inputs.fields_container.connect(disp_fc)
+mapping_operator.inputs.coordinates.connect(field_coord)
+mapping_operator.inputs.mesh.connect(mesh)
+mapping_operator.inputs.create_support.connect(True)
+fields_mapped = mapping_operator.outputs.fields_container()
+
+# Here, we request the mapped field data and its mesh
+field_m = fields_mapped[0]
+mesh_m = field_m.meshed_region
+
+# Now we create the plotter and add fields and meshes
+from ansys.dpf.core.plotter import DpfPlotter
+pl = DpfPlotter()
+
+pl.add_field(field_m, mesh_m)
+pl.add_mesh(mesh, style="surface", show_edges=True,
+             color="w", opacity=0.3)
+
+# Finally we plot the result
+pl.show_figure()
