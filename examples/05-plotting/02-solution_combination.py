06c67cafc967584b902c5f777e5dd8b2462ac4c0
Ayush Kumar <ayush.kumar@ansys.com>
2022-01-14 16:42:09 +0100

    updated as per pep8

--- examples/05-plotting/02-solution_combination.py
+++ examples/05-plotting/02-solution_combination.py
@@ -1,7 +1,8 @@
 from ansys.dpf import core as dpf
+from ansys.dpf.core import examples
 from ansys.dpf.core.plotter import DpfPlotter
 
-model = dpf.Model(r"D:\SRs\2022\SRSS_SolutionCombination\Solution combination_files\dp0\SYS\MECH\file.rst")
+model = dpf.Model(examples.msup_transient)
 
 # Instantiate S1
 stress_tensor = model.results.stress()
@@ -20,10 +21,12 @@ LC2: stress_tensor.outputs.fields_container.get_data()[1]
 """
 
 # Scale LC2 to -1
+field_lc2 = stress_tensor.outputs.fields_container.get_data()[1]
 stress_tensor_lc2_sc = dpf.operators.math.scale(field=stress_tensor.outputs.fields_container.get_data()[1],
                                                 ponderation=-1.0)
 # Add load cases
-stress_tensor_combi = dpf.operators.math.add(fieldA=stress_tensor.outputs.fields_container.get_data()[0],
+field_lc1 = stress_tensor.outputs.fields_container.get_data()[0]
+stress_tensor_combi = dpf.operators.math.add(fieldA=field_lc1,
                                              fieldB=stress_tensor_lc2_sc)
 
 # Principal Stresses => Eigenvalues of the new stress tensor
