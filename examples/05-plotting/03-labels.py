c2cf28d75cbaeddc33366b80d3913322c7aa4b73
Ayush Kumar <ayush.kumar@ansys.com>
2022-01-18 15:28:03 +0100

    formatted examples

--- examples/05-plotting/03-labels.py
+++ examples/05-plotting/03-labels.py
@@ -3,27 +3,47 @@
 
 Add Nodal Labels on Plots.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
-One can custom labels to specific nodes with specific label properties.
+You can custom labels to specific nodes with specific label properties.
 If label for a node is missing, by default nodal scalar value is shown.
 """
-
+###############################################################################
+# First, import the DPF-Core module as ``dpf_core`` and import the
+# included examples file and ``DpfPlotter``
 from ansys.dpf import core as dpf
 from ansys.dpf.core import examples
 from ansys.dpf.core.plotter import DpfPlotter
-
+###############################################################################
+# Next, open an example and print out the ``model`` object.  The
+# ``Model`` class helps to organize access methods for the result by
+# keeping track of the operators and data sources used by the result
+# file.
+#
+# Printing the model displays:
+#
+# - Analysis type
+# - Available results
+# - Size of the mesh
+# - Number of results
+#
 model = dpf.Model(examples.msup_transient)
-
-# Get stress tensor
+print(model)
+###############################################################################
+# Get the stress tensor and connect time scoping.
+# Make sure to define ``Nodal`` as requested location,
+# as the labels are supported only for Nodal results.
+#
 stress_tensor = model.results.stress()
 time_scope = dpf.Scoping()
 time_scope.ids = [1, 2]
 stress_tensor.inputs.time_scoping.connect(time_scope)
 stress_tensor.inputs.requested_location.connect("Nodal")
-
-# Get the meshed region
+###############################################################################
+#  Get the meshed region
+#
 mesh_set = model.metadata.meshed_region
-
+###############################################################################
 # Plot the results on the mesh, show Min and Max
+#
 plot = DpfPlotter()
 plot.add_field(stress_tensor.outputs.fields_container.get_data()[1],
                meshed_region=mesh_set,
@@ -31,12 +51,10 @@ plot.add_field(stress_tensor.outputs.fields_container.get_data()[1],
                label_text_size=15,
                label_point_size=5,
                )
-
-"""
-Add custom labels to specific nodes with specific label properties.
-If label for a node is missing, by default nodal value is shown.
-"""
-
+###############################################################################
+# Add custom labels to specific nodes with specific label properties.
+# If label for a node is missing, by default nodal value is shown.
+#
 my_nodes_1 = [mesh_set.nodes[0], mesh_set.nodes[10]]
 my_labels_1 = ["MyNode1", "MyNode2"]
 plot.add_node_labels(my_nodes_1, mesh_set, my_labels_1,
@@ -51,6 +69,7 @@ plot.add_node_labels(my_nodes_2, mesh_set, my_labels_2,
                      font_size=30, text_color="yellow",
                      font_family="arial", shadow=False,
                      point_color="white", point_size=30)
-
+###############################################################################
 # Show figure
+#
 plot.show_figure(show_axes=True)
