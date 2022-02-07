855a27a4333cf947e96e9ff43f665ed09a5b9ee6
cbellot <camille.bellot@ansys.com>
2022-02-07 09:34:50 +0100

    fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from io import open as io_open
 
 from setuptools import setup
 
-install_requires = ["psutil", "progressbar2", "numpy", "ansys.grpc.dpf>=0.2.3, <0.4.0"]
+install_requires = ["psutil", "progressbar2", "numpy", "ansys.grpc.dpf>=0.2.3"]
 
 
 # Get version from version info
