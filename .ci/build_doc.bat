d68442ad99ce48c13e4eeef5123a1e4ac065c203
cbellot <camille.bellot@ansys.com>
2022-02-10 08:15:24 +0100

    fix ci

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -1,7 +1,7 @@
 set SPHINX_APIDOC_OPTIONS=inherited-members
-call sphinx-apidoc -o ../docs/source/api ../ansys ../ansys/dpf/core/aeneid.py -f --implicit-namespaces --separate  --no-headings
+call sphinx-apidoc -o ../docs/source/api ../ansys -f --implicit-namespaces --separate  --no-headings
 pushd .
 cd ../docs/
 call make clean
-call make html SPHINXOPTS="-w build_errors.txt -N"
+call make html
 popd
