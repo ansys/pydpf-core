e37028d7ea704dd06e0e331d0b2d74b1788196ae
jose-henriquezroa <jose.henriquezroa@ansys.com>
2022-04-11 09:35:28 +0200

    Removed make clean

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -2,6 +2,6 @@ set SPHINX_APIDOC_OPTIONS=inherited-members
 call sphinx-apidoc -o ../docs/source/api ../ansys -f --implicit-namespaces --separate  --no-headings
 pushd .
 cd ../docs/
-call make clean
+:: call make clean
 call make html
 popd
