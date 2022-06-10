db48fa96f61a05228d00078ab283d726fdc08a85
paul.profizi <paul.profizi@ansys.com>
2022-06-10 09:46:53 +0200

    Update the example to show "notebook=False" is required for screenshots on a Jupyter notebook.

--- examples/05-plotting/00-basic_plotting.py
+++ examples/05-plotting/00-basic_plotting.py
@@ -10,14 +10,18 @@ shown with the arguments available.
 
 from ansys.dpf import core as dpf
 from ansys.dpf.core import examples
-# from ansys.dpf.core.plotter import plot_chart
 
 
 # Plot the bare mesh of a model
 model = dpf.Model(examples.multishells_rst)
 model.plot(color="w", show_edges=True, title='Model', text='Model plot')
 # # Additional PyVista kwargs are supported, such as:
-model.plot(off_screen=True, screenshot='model_plot.png', title='Model', text='Model plot')
+model.plot(off_screen=True, notebook=False, screenshot='model_plot.png',
+           title='Model', text='Model plot')
+
+# Notes:
+# - To make screenshots, use "screenshot" as well as "notebook=False" if on a Jupyter notebook.
+# - The "off_screen" keyword only works when "notebook=False" to prevent the GUI from appearing.
 
 
 # Plot a field on its supporting mesh (field location must be Elemental or Nodal)
@@ -27,14 +31,16 @@ fc = stress.outputs.fields_container()
 field = fc[0]
 field.plot(notebook=False, shell_layers=None, show_axes=True, title='Field', text='Field plot')
 # # Additional PyVista kwargs are supported, such as:
-field.plot(off_screen=True, screenshot='field_plot.png', title='Field', text='Field plot off')
+field.plot(off_screen=True, notebook=False, screenshot='field_plot.png',
+           title='Field', text='Field plot off')
 #
 # # Alternatively one can plot the MeshedRegion associated to the model
 mesh = model.metadata.meshed_region
 mesh.plot(field_or_fields_container=None, shell_layers=None, show_axes=True,
           title='Mesh fc None', text='Mesh plot')
 # Additional PyVista kwargs are supported, such as:
-mesh.plot(off_screen=True, screenshot='mesh_plot.png', title='Mesh', text='Mesh plot off')
+mesh.plot(off_screen=True, notebook=False, screenshot='mesh_plot.png',
+          title='Mesh', text='Mesh plot off')
 # A fields_container or a specific field can be given to plot on the mesh.
 mesh.plot(field_or_fields_container=fc, title='Mesh with fields container', text='Mesh fc plot')
 mesh.plot(field_or_fields_container=field, title='Mesh with field', text='Mesh field plot')
@@ -53,5 +59,5 @@ disp_op.connect(4, ds)
 disp_fc = disp_op.outputs.fields_container()
 meshes_cont.plot(disp_fc, title='Meshes Container disp_fc', text='Meshes Container disp_fc plot')
 # Additional PyVista kwargs are supported, such as:
-meshes_cont.plot(off_screen=True, screenshot='meshes_cont_plot.png',
+meshes_cont.plot(off_screen=True, notebook=False, screenshot='meshes_cont_plot.png',
                  title='Meshes Container', text='Meshes Container plot')
