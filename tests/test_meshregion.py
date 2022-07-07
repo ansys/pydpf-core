96391de25c8ad4986d968ce4a014160693d4720c
paul.profizi <paul.profizi@ansys.com>
2022-07-07 14:55:34 +0200

    Add test for nodes.nodal_connectivity_field.setter

--- tests/test_meshregion.py
+++ tests/test_meshregion.py
@@ -272,6 +272,12 @@ def test_connectivity_meshed_region():
     assert np.allclose(nodal_conne.get_entity_data_by_id(1), [0])
     assert np.allclose(mesh.nodes.node_by_id(1).nodal_connectivity, [0])
 
+    new_connectivity_data = nodal_conne.data
+    new_connectivity_data[0] = 1
+    nodal_conne.data = new_connectivity_data
+    mesh.nodes.nodal_connectivity_field = nodal_conne
+    assert np.allclose(nodal_conne.get_entity_data_by_id(1), [0])
+    assert np.allclose(mesh.nodes.node_by_id(1).nodal_connectivity, [0])
 
 
 def test_create_all_shaped_meshed_region():
