0f3e756ad4c623b3d3cd8dc6252e13334ea85cb4
paul.profizi <paul.profizi@ansys.com>
2022-07-07 11:03:02 +0200

    Delete build_doc.bat

--- .ci/build_doc.bat
+++
@@ -1,7 +0,0 @@
-set SPHINX_APIDOC_OPTIONS=inherited-members
-call sphinx-apidoc -o ../docs/source/api ../ansys -f --implicit-namespaces --separate  --no-headings
-pushd .
-cd ../docs/
-call make clean
-call make html
-popd
