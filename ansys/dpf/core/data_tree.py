8e7334f928a32816fb877a2a9e1d3e646c0cc8a8
Camille Bellot <80476446+cbellot000@users.noreply.github.com>
2022-07-19 09:57:25 +0200

    Merge/0.5.dev2 code fixes to remove warnings (#311)
    * fixes
    
    * fix deleters field and servers
    
    * remove Scooby
    
    * fix deleters
    
    * add fixture to restart local server
    
    * revert codacy fixes
    
    * flake8 styling

--- ansys/dpf/core/data_tree.py
+++ ansys/dpf/core/data_tree.py
@@ -400,7 +400,7 @@ class DataTree:
         """
         return self._api.dpf_data_tree_has_attribute(self, entry)
 
-    def get_as(self, name, type=types.string):
+    def get_as(self, name, type_to_return=types.string):
         """
         Returns an attribute value by its name in the required type.
 
@@ -408,7 +408,7 @@ class DataTree:
         ----------
         name : str
             Name of the attribute to return
-        type : types
+        type_to_return : types
             Type of the attribute to return. String is supported for all attributes.
 
         Returns
@@ -429,28 +429,28 @@ class DataTree:
 
         """
         out = None
-        if type == types.int:
+        if type_to_return == types.int:
             out = integral_types.MutableInt32()
             self._api.dpf_data_tree_get_int_attribute(self, name, out)
             out = int(out)
-        elif type == types.double:
+        elif type_to_return == types.double:
             out = integral_types.MutableDouble()
             self._api.dpf_data_tree_get_double_attribute(self, name, out)
             out = float(out)
-        elif type == types.string:
+        elif type_to_return == types.string:
             out = integral_types.MutableString(1)
             size = integral_types.MutableInt32(0)
             self._api.dpf_data_tree_get_string_attribute(self, name, out, size)
             out = str(out)
-        elif type == types.vec_double:
+        elif type_to_return == types.vec_double:
             out = integral_types.MutableListDouble()
             self._api.dpf_data_tree_get_vec_double_attribute(self, name, out, out.internal_size)
             out = out.tolist()
-        elif type == types.vec_int:
+        elif type_to_return == types.vec_int:
             out = integral_types.MutableListInt32()
             self._api.dpf_data_tree_get_vec_int_attribute(self, name, out, out.internal_size)
             out = out.tolist()
-        elif type == types.vec_string:
+        elif type_to_return == types.vec_string:
             coll_obj = object_handler.ObjHandler(
                 data_processing_api=self._core_api,
                 internal_obj=self._api.dpf_data_tree_get_string_collection_attribute(self, name)
@@ -459,7 +459,7 @@ class DataTree:
             out = []
             for i in range(num):
                 out.append(self._coll_api.collection_get_string_entry(coll_obj, i))
-        elif type == types.data_tree:
+        elif type_to_return == types.data_tree:
             obj = self._api.dpf_data_tree_get_sub_tree(self, name)
             out = DataTree(data_tree=obj, server=self._server)
         return out
@@ -518,7 +518,7 @@ class _LocalDataTree(DataTree):
     def __enter__(self):
         return self
 
-    def __exit__(self, type, value, tb):
+    def __exit__(self, type_to_use, value, tb):
         if tb is None:
             self._is_exited = True
             self.release_data()
@@ -529,4 +529,3 @@ class _LocalDataTree(DataTree):
         if not hasattr(self, "_is_exited") or not self._is_exited:
             self._is_exited = True
             self.release_data()
-        pass
--- ansys/dpf/core/misc.py
+++ ansys/dpf/core/misc.py
@@ -11,19 +11,6 @@ RETURN_ARRAYS = True
 
 RUNTIME_CLIENT_CONFIG = None
 
-# ANSYS CPython Workbench environment may not have scooby installed.
-try:
-    from scooby import Report as ScoobyReport
-except ImportError:
-
-    class ScoobyReport:
-        """Placeholder for Scooby report."""
-
-        def __init__(self, *args, **kwargs):
-            raise ImportError(
-                "Install `scooby` with `pip install scooby` to use " "this feature"
-            )
-
 
 def module_exists(module_name):
     """Check if a model exists.
@@ -35,59 +22,6 @@ def module_exists(module_name):
 
     """
     return module_name in (name for loader, name, ispkg in iter_modules())
-
-
-class Report(ScoobyReport):
-    """Generate a report of the installed packages for DPF-Core."""
-
-    def __init__(self, additional=None, ncol=3, text_width=80, sort=False, gpu=True):
-        """Generate a :class:`scooby.Report` instance.
-
-        Parameters
-        ----------
-        additional : list(ModuleType), list(str)
-            List of packages or package names to add to the output information.
-        ncol : int, optional
-            Number of package columns in the HTML table. This parameter has effect
-            only if ``mode='HTML'`` or ``mode='html'``. The default is ``3``.
-        text_width : int, optional
-            Width of the text for non-HTML display modes. The default is ``80``.
-        sort : bool, optional
-            Whether to sort the packages alphabetically. The default is "False``.
-        gpu : bool, optional
-            Whether to gather information about the GPU. The default is ``True``.
-            If rendering issues are experiencd, set to ``False`` to safely generate
-            the report.
-
-        """
-
-        # Mandatory packages.
-        core = ["ansys.grpc.dpf"]
-
-        # Optional packages.
-        optional = []
-
-        # Information about the GPU - bare except in case there is a rendering
-        # bug that the user is trying to report.
-        if gpu:
-            from pyvista.utilities.errors import GPUInfo
-
-            try:
-                extra_meta = [(t[1], t[0]) for t in GPUInfo().get_info()]
-            except:
-                extra_meta = ("GPU Details", "error")
-        else:
-            extra_meta = ("GPU Details", "None")
-
-        super().__init__(
-            additional=additional,
-            core=core,
-            optional=optional,
-            ncol=ncol,
-            text_width=text_width,
-            sort=sort,
-            extra_meta=extra_meta,
-        )
 
 
 def is_float(string):
