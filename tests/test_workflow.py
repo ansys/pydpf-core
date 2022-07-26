a133d1b485ccf6028fb256312bc494829698972a
Camille Bellot <80476446+cbellot000@users.noreply.github.com>
2022-07-26 17:11:29 +0200

    Merge/retro/0.5.dev2 fix workflow (#344)
    * fixes
    
    * debug
    
    * restart server
    
    * Revert "debug"
    
    This reverts commit 2ce67398ea022d766e2276ebc2778f7c26d277f3.
    
    * update wheels
    
    * comment

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -655,8 +655,6 @@ def test_create_on_other_server2_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_with_ip_workflow(local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
@@ -674,8 +672,7 @@ def test_create_on_other_server_with_ip_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_with_address_workflow(local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
+    dpf.core.start_local_server()
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
@@ -692,8 +689,6 @@ def test_create_on_other_server_with_address_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_with_address2_workflow(local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
@@ -710,8 +705,6 @@ def test_create_on_other_server_with_address2_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_and_connect_workflow(allkindofcomplexity, local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
