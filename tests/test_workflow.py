cac07354707008b380af10dcb0acfe37a5851145
cbellot <camille.bellot@ansys.com>
2022-07-26 15:42:41 +0200

    fixes

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -655,8 +655,6 @@ def test_create_on_other_server2_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_with_ip_workflow(local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
@@ -674,8 +672,6 @@ def test_create_on_other_server_with_ip_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_with_address_workflow(local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
@@ -692,8 +688,6 @@ def test_create_on_other_server_with_address_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_with_address2_workflow(local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
@@ -710,8 +704,6 @@ def test_create_on_other_server_with_address2_workflow(local_server):
 @pytest.mark.xfail(raises=dpf.core.errors.ServerTypeError)
 @conftest.raises_for_servers_version_under('3.0')
 def test_create_on_other_server_and_connect_workflow(allkindofcomplexity, local_server):
-    conftest.local_servers.clear()
-    local_server = conftest.local_servers[0]
     disp_op = op.result.displacement()
     max_fc_op = op.min_max.min_max_fc(disp_op)
     workflow = dpf.core.Workflow()
