30807c292efd4c3f8e6923ec4a12631de2d80903
cbellot <camille.bellot@ansys.com>
2022-07-27 11:10:32 +0200

    fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -275,26 +275,6 @@ if SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0:
     )
     def server_type_legacy_grpc(request):
         return core.start_local_server(config=request.param, as_global=False)
-
-    @pytest.fixture(scope="session", params=[ServerConfig(protocol=CommunicationProtocols.gRPC,
-                                                          legacy=True),
-                                             ServerConfig(protocol=CommunicationProtocols.gRPC,
-                                                          legacy=False)],
-                    ids=[
-                        "ansys-grpc-dpf config",
-                        "gRPC CLayer config"
-                    ])
-    def remote_config_server_type(request):
-        return request.param
-
-    @pytest.fixture(scope="session", params=[ServerConfig(protocol=CommunicationProtocols.gRPC,
-                                                          legacy=True)],
-                    ids=[
-                        "ansys-grpc-dpf",
-                    ])
-    def server_type_legacy_grpc(request):
-        return core.start_local_server(config=request.param, as_global=False)
-
 else:
 
     @pytest.fixture(scope="session")
