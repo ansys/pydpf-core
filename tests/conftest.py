4a30ad06c5f62a813454898bb879c7af3f0d63ac
paul.profizi <paul.profizi@ansys.com>
2022-07-28 18:16:37 +0200

    Remove the unnecessary commented lines

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,8 +21,7 @@ import warnings
 ACCEPTABLE_FAILURE_RATE = 0
 
 core.settings.disable_off_screen_rendering()
-# os.environ["PYVISTA_OFF_SCREEN"] = "true"
-# os.environ["MPLBACKEND"] = "Agg"
+
 # currently running dpf on docker.  Used for testing on CI
 running_docker = ansys.dpf.core.server_types.RUNNING_DOCKER["use_docker"]
 
