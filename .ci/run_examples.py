290f6ee6cf6d291cc5128b3d82c784b6f5520705
cbellot <camille.bellot@ansys.com>
2022-07-28 13:39:11 +0200

    testing

--- .ci/run_examples.py
+++ .ci/run_examples.py
@@ -12,15 +12,6 @@ actual_path = pathlib.Path(__file__).parent.absolute()
 print(os.path.join(actual_path, os.path.pardir, "examples"))
 
 
-DPF_SERVER_TYPE = os.environ.get("DPF_SERVER_TYPE", None)
-if DPF_SERVER_TYPE:
-    if DPF_SERVER_TYPE == "INPROCESS":
-        core.SERVER_CONFIGURATION = core.AvailableServerConfigs.InProcessServer
-    elif DPF_SERVER_TYPE == "GRPC":
-        core.SERVER_CONFIGURATION = core.AvailableServerConfigs.GrpcServer
-    elif DPF_SERVER_TYPE == "LEGACYGRPC":
-        core.SERVER_CONFIGURATION = core.AvailableServerConfigs.LegacyGrpcServer
-
 for root, subdirectories, files in os.walk(os.path.join(actual_path, os.path.pardir, "examples")):
     for subdirectory in subdirectories:
         subdir = os.path.join(root, subdirectory)
--- .ci/run_non_regression_examples.py
+++ .ci/run_non_regression_examples.py
@@ -11,14 +11,6 @@ os.environ["MPLBACKEND"] = "Agg"
 actual_path = pathlib.Path(__file__).parent.absolute()
 print(os.path.join(actual_path, os.path.pardir, "examples"))
 
-DPF_SERVER_TYPE = os.environ.get("DPF_SERVER_TYPE", None)
-if DPF_SERVER_TYPE:
-    if DPF_SERVER_TYPE == "INPROCESS":
-        core.SERVER_CONFIGURATION = core.AvailableServerConfigs.InProcessServer
-    elif DPF_SERVER_TYPE == "GRPC":
-        core.SERVER_CONFIGURATION = core.AvailableServerConfigs.GrpcServer
-    elif DPF_SERVER_TYPE == "LEGACYGRPC":
-        core.SERVER_CONFIGURATION = core.AvailableServerConfigs.LegacyGrpcServer
 
 list_tests = [
     os.path.join(actual_path, os.path.pardir, "examples", "00-basic"),
