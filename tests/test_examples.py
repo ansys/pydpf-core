461f679d2f304db6efd2e8914704767ea00f5aac
vsousacosta <vinicius.costa@ansys.com>
2022-08-01 16:02:39 +0200

    add 01-average_across_bodies.py example

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -21,8 +21,14 @@ def test_download_example_asme_result():
     path = examples.download_example_asme_result()
     assert isinstance(Model(path), Model)
 
+
 def test_download_crankshaft():
     path = examples.download_crankshaft()
+    assert isinstance(Model(path), Model)
+
+
+def test_download_piston_rod():
+    path = examples.download_piston_rod()
     assert isinstance(Model(path), Model)
 
 
