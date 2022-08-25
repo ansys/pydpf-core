ea78875251b78089f2ba8878d7871812aff1de0c
PProfizi <paul.profizi@ansys.com>
2022-08-25 09:23:18 +0200

    Revert "add copybutton (#368)"
    This reverts commit f2a723d2d42122d0ec424b88d6583db74b94b1de.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,17 +44,16 @@ release = __version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "enum_tools.autoenum",
-    "nbsphinx",
-    "pydata_sphinx_theme",
     "sphinx.ext.autodoc",
-    "sphinx.ext.graphviz",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.todo",
-    "sphinx_copybutton",
-    "sphinx_design",
     "sphinx_gallery.gen_gallery",
+    "sphinx.ext.todo",
+    "sphinx.ext.napoleon",
+    "pydata_sphinx_theme",
+    "sphinx_design",
+    "nbsphinx",
+    "sphinx.ext.graphviz",
+    "enum_tools.autoenum",
+    "sphinx.ext.intersphinx"
 ]
 
 # Intersphinx mapping
