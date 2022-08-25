b7b69c0c4409c22b6bf1758ee7a28dc603caee8e
Ayush Kumar <ayush.kumar@ansys.com>
2022-08-25 08:05:21 +0200

    implemented review suggestions

--- examples/00-basic/12-get_material_properties.py
+++ examples/00-basic/12-get_material_properties.py
@@ -5,10 +5,12 @@ Get material properties from the result file
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Material properties are assigned to each element in APDL and by default they
 are written out in the APDL result file. We can extract material properties
-of each element using PyDPF by getting the properties from the `meshed_region`.
+of each element using PyDPF by getting the properties from the
+:class:`meshed_region <ansys.dpf.core.meshed_region.MeshedRegion>`.
 
 The material properties are then passed onto the operator
-`mapdl_material_properties()`
+:class:`mapdl_material_properties
+<ansys.dpf.core.operators.result.mapdl_material_properties.mapdl_material_properties>`
 
 Import necessary modules:
 """
@@ -21,12 +23,14 @@ from ansys.dpf.core import examples
 model = dpf.Model(examples.simple_bar)
 
 ###############################################################################
-# Get the `meshed_region` from model's metadata.
+# Get the :class:`meshed_region <ansys.dpf.core.meshed_region.MeshedRegion>`
+# from model's metadata.
 mesh = model.metadata.meshed_region
 print(mesh)
 
 ###############################################################################
-# See available properties in the `meshed_region`
+# See available properties in the :class:`meshed_region
+# <ansys.dpf.core.meshed_region.MeshedRegion>`
 print(mesh.available_property_fields)
 
 ###############################################################################
@@ -34,16 +38,17 @@ print(mesh.available_property_fields)
 mats = mesh.property_field("mat")
 
 ###############################################################################
-# Use the DPF operator `mapdl_material_properties` to extract data for the
-# materials - `mats`. For the input `properties_name`, you need the correct
-# material property string. To see what all strings are supported, you can
-# print the operator help.
+# Use the DPF operator :class:`mapdl_material_properties
+# <ansys.dpf.core.operators.result.mapdl_material_properties.mapdl_material_properties>`
+# to extract data for the # materials - `mats`. For the input
+# `properties_name`, you need the correct material property string. To see
+# which strings are supported, you can print the operator help.
 mat_prop = model.operator("mapdl_material_properties")
 mat_prop.inputs.materials.connect(mats)
 
 ###############################################################################
-# For the input `properties_name`, you need the correct
-# material property string. To see what all strings are supported, you can
+# For the input pin `properties_name`, you need the correct
+# material property string. To see which strings are supported, you can
 # print the operator help.
 print(mat_prop)
 
