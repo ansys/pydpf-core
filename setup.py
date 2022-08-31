5b1e35a842d3a1666d52e2c53d0a8eb9e860349f
paul.profizi <paul.profizi@ansys.com>
2022-08-31 12:27:22 +0200

    Fix ansys-dpf-gate version dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from io import open as io_open
 
 from setuptools import setup
 
-install_requires = ["packaging", "psutil", "tqdm", "numpy", "ansys-dpf-gate>0.1.*"]
+install_requires = ["packaging", "psutil", "tqdm", "numpy", "ansys-dpf-gate>=0.2.*"]
 
 # Get version from version info
 filepath = os.path.dirname(__file__)
