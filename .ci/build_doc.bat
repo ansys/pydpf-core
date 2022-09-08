12201575107a2367f020621278556210012ff5d4
PProfizi <paul.profizi@ansys.com>
2022-09-08 17:18:59 +0200

    Revert changes to build_doc.bat

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -4,7 +4,7 @@ call sphinx-apidoc -o ../docs/source/api ../ansys ../ansys/dpf/core/log.py ^
 ../ansys/dpf/core/field_base.py ../ansys/dpf/core/cache.py ../ansys/dpf/core/misc.py ^
 ../ansys/dpf/core/check_version.py ../ansys/dpf/core/operators/build.py ../ansys/dpf/core/operators/specification.py ^
 ../ansys/dpf/core/vtk_helper.py ^
- -f --implicit-namespaces --separate  --no-headings -v -v -v
+ -f --implicit-namespaces --separate  --no-headings
 pushd .
 cd ../docs/
 call make clean
