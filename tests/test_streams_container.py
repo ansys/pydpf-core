699947596c173c86742ea650236f5a2f59d66e18
PProfizi <paul.profizi@ansys.com>
2022-09-09 14:48:53 +0200

    Revert "Test support for all server configurations. To debug."
    This reverts commit 4866247c86f389e346abe99c1f3df90bd9c6c70d.

--- tests/test_streams_container.py
+++ tests/test_streams_container.py
@@ -3,20 +3,25 @@ import pytest
 import shutil
 
 from ansys import dpf
+from conftest import SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0
 
 
-def test_create_streams_container(server_type, simple_bar):
-    model = dpf.core.Model(simple_bar, server=server_type)
+@pytest.mark.skipif(not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0,
+                    reason='Unavailable in 221')
+def test_create_streams_container(simple_bar):
+    model = dpf.core.Model(simple_bar)
     streams_provider = model.metadata.streams_provider
     sc = streams_provider.outputs.streams_container()
     assert hasattr(sc, "release_handles")
 
 
-def test_release_handles(server_type, simple_bar):
+@pytest.mark.skipif(not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0,
+                    reason='Unavailable in 221')
+def test_release_handles(simple_bar):
     split = os.path.splitext(simple_bar)
     copy_path = split[0]+"copy"+split[1]
     shutil.copyfile(simple_bar, copy_path)
-    model = dpf.core.Model(copy_path, server=server_type)
+    model = dpf.core.Model(copy_path)
     # Evaluate something from the rst
     _ = model.metadata.meshed_region
     streams_provider = model.metadata.streams_provider
@@ -25,31 +30,38 @@ def test_release_handles(server_type, simple_bar):
     os.remove(copy_path)
 
 
-def test_release_streams_model(server_type, simple_bar):
+@pytest.mark.skipif(not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0,
+                    reason='Unavailable in 221')
+def test_release_streams_model(simple_bar):
     split = os.path.splitext(simple_bar)
     copy_path = split[0]+"copy2"+split[1]
     shutil.copyfile(simple_bar, copy_path)
-    model = dpf.core.Model(copy_path, server=server_type)
+    model = dpf.core.Model(copy_path)
     # Evaluate something from the rst
     _ = model.metadata.meshed_region
     model.metadata.release_streams()
     os.remove(copy_path)
 
 
-def test_release_streams_model_empty(server_type):
-    model = dpf.core.Model(server=server_type)
+@pytest.mark.skipif(not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0,
+                    reason='Unavailable in 221')
+def test_release_streams_model_empty():
+    model = dpf.core.Model()
     model.metadata.release_streams()
 
 
-def test_create_from_streams_container(server_type, simple_bar):
-    model = dpf.core.Model(simple_bar, server=server_type)
+@pytest.mark.skipif(not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0,
+                    reason='Unavailable in 221')
+def test_create_from_streams_container(server_clayer, simple_bar):
+    model = dpf.core.Model(simple_bar)
     streams_provider = model.metadata.streams_provider
     sc = streams_provider.outputs.streams_container()
-    dpf.core.streams_container.StreamsContainer(streams_container=sc,
-                                                server=server_type)
+    dpf.core.streams_container.StreamsContainer(streams_container=sc)
 
 
-# def test_create_streams_container_raise_legacy(server_type_legacy_grpc):
-#     with pytest.raises(NotImplementedError):
-#         dpf.core.streams_container.StreamsContainer(
-#             server=server_type_legacy_grpc)
+@pytest.mark.skipif(not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0,
+                    reason='Unavailable in 221')
+def test_create_streams_container_raise_legacy(server_type_legacy_grpc):
+    with pytest.raises(NotImplementedError):
+        dpf.core.streams_container.StreamsContainer(
+            server=server_type_legacy_grpc)
