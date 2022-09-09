298e737bc2754470e6ba0ebee14091c468b2f2bb
PProfizi <paul.profizi@ansys.com>
2022-09-09 10:57:41 +0200

    Revert "Comment-out modal-harmonic examples"
    This reverts commit a0a50681f69285367ccc1a34a36f9cb2862dbe61.

--- examples/02-modal-harmonic/03-compare_modes.py
+++ examples/02-modal-harmonic/03-compare_modes.py
@@ -8,34 +8,34 @@ helpers to request results on specific mesh and time scopings.
 With those helpers, working on a custom spatial and temporal subset of the
 model is straightforward.
 """
-#
-# from ansys.dpf import core as dpf
-# from ansys.dpf.core import examples
-#
-# ###############################################################################
-# # First, create a model object to establish a connection with an
-# # example result file
-# model = dpf.Model(examples.download_all_kinds_of_complexity_modal())
-# print(model)
-#
-# ###############################################################################
-# # Visualize specific mode shapes
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# # Choose the modes to visualize
-# modes = [1, 5, 10, 15]
-#
-# ###############################################################################
-# # Choose to split the displacement on solid/shell/beam to only focus on shell
-# # elements
-# disp = model.results.displacement
-# for mode in modes:
-#     fc = disp.on_time_scoping(mode).split_by_shape.eval()
-#     model.metadata.meshed_region.plot(fc.shell_field())
-#
-# ###############################################################################
-# # Choose to split the displacement on solid/shell/beam to only focus on solid
-# # elements
-# disp = model.results.displacement
-# for mode in modes:
-#     fc = disp.on_time_scoping(mode).split_by_shape.eval()
-#     model.metadata.meshed_region.plot(fc.solid_field())
+
+from ansys.dpf import core as dpf
+from ansys.dpf.core import examples
+
+###############################################################################
+# First, create a model object to establish a connection with an
+# example result file
+model = dpf.Model(examples.download_all_kinds_of_complexity_modal())
+print(model)
+
+###############################################################################
+# Visualize specific mode shapes
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Choose the modes to visualize
+modes = [1, 5, 10, 15]
+
+###############################################################################
+# Choose to split the displacement on solid/shell/beam to only focus on shell
+# elements
+disp = model.results.displacement
+for mode in modes:
+    fc = disp.on_time_scoping(mode).split_by_shape.eval()
+    model.metadata.meshed_region.plot(fc.shell_field())
+
+###############################################################################
+# Choose to split the displacement on solid/shell/beam to only focus on solid
+# elements
+disp = model.results.displacement
+for mode in modes:
+    fc = disp.on_time_scoping(mode).split_by_shape.eval()
+    model.metadata.meshed_region.plot(fc.solid_field())
