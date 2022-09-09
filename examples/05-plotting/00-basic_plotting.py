55154ae147491bb5a25e63da16c25f40c4a8f43e
PProfizi <paul.profizi@ansys.com>
2022-09-09 19:17:21 +0200

    Try the first plot command the 00-basic_plotting.py example

--- examples/05-plotting/00-basic_plotting.py
+++ examples/05-plotting/00-basic_plotting.py
@@ -14,7 +14,7 @@ from ansys.dpf.core import examples
 
 # Plot the bare mesh of a model
 model = dpf.Model(examples.multishells_rst)
-# model.plot(color="w", show_edges=True, title='Model', text='Model plot')
+model.plot(color="w", show_edges=True, title='Model', text='Model plot')
 # # # Additional PyVista kwargs are supported, such as:
 # model.plot(off_screen=True, notebook=False, screenshot='model_plot.png',
 #            title='Model', text='Model plot off')
