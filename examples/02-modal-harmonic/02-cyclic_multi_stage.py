b47ba02756b6c62f1b231ef57e3952ac12f9496f
PProfizi <paul.profizi@ansys.com>
2022-09-12 16:23:42 +0200

    Revert "Delete modal-harmonic examples"
    This reverts commit 580543890f1a4af965f896f130cd87e4d4fab5c3.

---
+++ examples/02-modal-harmonic/02-cyclic_multi_stage.py
@@ -0,0 +1,63 @@
+"""
+.. _ref_multi_stage_cyclic:
+
+Multi-stage cyclic symmetry example
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This example shows how to expand the mesh and results from a
+multi-stage cyclic analysis.
+
+"""
+from ansys.dpf import core as dpf
+from ansys.dpf.core import examples
+
+###############################################################################
+# Create the model and display the state of the result.
+cyc = examples.download_multi_stage_cyclic_result()
+model = dpf.Model(cyc)
+print(model)
+
+###############################################################################
+# Expand displacement results
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# This example expands displacement results, by default on all
+# nodes and the first time step.
+
+# Create displacement cyclic operator
+UCyc = model.results.displacement()
+UCyc.inputs.read_cyclic(2)
+
+# expand the displacements and get a total deformation
+nrm = dpf.Operator("norm_fc")
+nrm.inputs.connect(UCyc.outputs)
+fields = nrm.outputs.fields_container()
+
+# # get the expanded mesh
+mesh_provider = model.metadata.mesh_provider
+mesh_provider.inputs.read_cyclic(2)
+mesh = mesh_provider.outputs.mesh()
+
+# # plot the expanded result on the expanded mesh
+mesh.plot(fields)
+
+###############################################################################
+# Expand stresses at a given time step
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# define stress expansion operator and request stresses at time set = 3
+SCyc = model.results.stress()
+SCyc.inputs.read_cyclic(2)
+SCyc.inputs.time_scoping.connect([3])
+
+# request the results averaged on the nodes
+SCyc.inputs.requested_location.connect("Nodal")
+
+# request equivalent von mises operator and connect it to stress
+# operator
+eqv = dpf.Operator("eqv_fc")
+eqv.inputs.connect(SCyc.outputs)
+
+# expand the results and get stress eqv
+fields = eqv.outputs.fields_container()
+
+# plot the expanded result on the expanded mesh
+mesh.plot(fields)
---
+++ examples/02-modal-harmonic/04-modal_superposition.py
@@ -0,0 +1,44 @@
+"""
+.. _ref_msup:
+
+Expand harmonic modal superposition with DPF
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Different types of linear dynamics expansions are implemented in DPF.
+With modal superposition used in harmonic analysis, modal coefficients
+are multiplied by mode shapes (of a previous modal analysis) to analyse
+a structure under given boundary conditions in a range of frequencies.
+Doing this expansion "on demand" in DPF instead of in the solver
+reduces the size of the result files.
+"""
+
+from ansys.dpf import core as dpf
+from ansys.dpf.core import examples
+
+###############################################################################
+# Create data sources
+# ~~~~~~~~~~~~~~~~~~~
+# Create data sources with the mode shapes and the modal response.
+# The expansion is recursive in DPF: first the modal response is read.
+# Then, "upstream" mode shapes are found in the data sources, where they
+# are read and expanded (mode shapes x modal response)
+
+msup_files = examples.download_msup_files_to_dict()
+data_sources = dpf.DataSources(msup_files["rfrq"])
+up_stream_data_sources = dpf.DataSources(msup_files["mode"])
+up_stream_data_sources.add_file_path(msup_files["rst"])
+
+data_sources.add_upstream(up_stream_data_sources)
+
+###############################################################################
+# Compute displacements
+# ~~~~~~~~~~~~~~~~~~~~~
+# Once the ``add_upstream()`` method puts the recursivity in the data sources,
+# in a harmonic, transient, or modal analysis, computing displacements with
+# or without expansion has the exact same syntax.
+
+model = dpf.Model(data_sources)
+disp = model.results.displacement.on_all_time_freqs.eval()
+
+freq_scoping = disp.get_time_scoping()
+for freq_set in freq_scoping:
+    model.metadata.meshed_region.plot(disp.get_field_by_time_complex_ids(freq_set, 0))
