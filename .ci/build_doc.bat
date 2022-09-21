284818421d37f917d1e6e346116b7b18a2216541
paul.profizi <paul.profizi@ansys.com>
2022-09-21 16:57:12 +0200

    Omit this operator when generating the sphinx-apidoc

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -3,7 +3,7 @@ call sphinx-apidoc -o ../docs/source/api ../ansys ../ansys/dpf/core/log.py ^
 ../ansys/dpf/core/help.py ../ansys/dpf/core/mapping_types.py ../ansys/dpf/core/ipconfig.py ^
 ../ansys/dpf/core/field_base.py ../ansys/dpf/core/cache.py ../ansys/dpf/core/misc.py ^
 ../ansys/dpf/core/check_version.py ../ansys/dpf/core/operators/build.py ../ansys/dpf/core/operators/specification.py ^
-../ansys/dpf/core/vtk_helper.py ^
+../ansys/dpf/core/vtk_helper.py ../ansys/dpf/core/operators/mapping/create_sc_mapping_workflow.py ^
  -f --implicit-namespaces --separate  --no-headings
 pushd .
 cd ../docs/
