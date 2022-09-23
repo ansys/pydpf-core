aeeafc722f2e7337714fc8e64b4edf7f6a274566
PProfizi <paul.profizi@ansys.com>
2022-09-23 17:46:00 +0200

    WIP

--- ansys/dpf/core/materials_container.py
+++ ansys/dpf/core/materials_container.py
@@ -4,10 +4,12 @@ MaterialsContainer
 ==================
 Contains classes associated with the DPF MaterialsContainer.
 """
+import warnings
 # from ansys.dpf.core import material
 from ansys.dpf.core import server as server_module
+from ansys.dpf.core import errors
 from ansys.dpf.gate import (
-    materials_container_capi
+    materials_container_capi,
 )
 
 
@@ -16,16 +18,35 @@ class MaterialsContainer:
     def __init__(self, materials_container=None, server=None):
         # step 1: get server
         self._server = server_module.get_or_create_server(server)
+
+        if not self._server.meet_version("4.0"):
+            raise errors.DpfVersionNotSupported("4.0")
+
+        # step 2: get api
         self._api = self._server.get_api_for_type(
-            capi=materials_container_capi.ScopingCAPI,
-            grpcapi=materials_container_grpcapi.ScopingGRPCAPI
+            capi=materials_container_capi.MaterialsContainerCAPI,
+            grpcapi=None
         )
         # step3: init environment
         self._api.init_materials_container_environment(self)  # creates stub when gRPC
 
-        # step2: if object exists, take the instance, else create it
-        if self._internal_obj is None:
-            if self._server.has_client():
-                self._internal_obj = self._api.collection_of_mesh_new_on_client(self._server.client)
-            else:
-                self._internal_obj = self._api.collection_of_mesh_new()
+        # step4: if object exists, take the instance, else create it
+        if materials_container is not None:
+            # init environment
+            pass
+        else:
+            # init environment
+            self._api.init_materials_container_environment(self)
+            # if self._server.has_client():
+            #     self._internal_obj = self._api.(self._server.client)
+            # else:
+            #     self._internal_obj = self._api.()
+
+    def __del__(self):
+        """Delete the entry."""
+        try:
+            # delete
+            if not self.owned:
+                self._deleter_func[0](self._deleter_func[1](self))
+        except:  # pylint: disable=bare-except
+            warnings.warn(traceback.format_exc())
