9ce044b07a49ef052f09ab4a6193493e7388e3de
PProfizi <paul.profizi@ansys.com>
2022-09-30 11:25:38 +0200

    Adapt to local

--- examples/09-fluids/cff.py
+++ examples/09-fluids/cff.py
@@ -10,15 +10,17 @@ available_operators = available_operator_names(server=server)
 cff_operators = [operator for operator in available_operators if "cff::" in operator]
 print(cff_operators)
 
-# Ok to plot all CFX models
-# Ok to plot all fluent models except for Polys (which is logical)
-# data_path = r"D:\ANSYSDev\Sandbox\cff\Ans.Dpf.CFF\Ans.Dpf.CFFTest\test_models\fluent\3D"
-# file_path = os.path.join(data_path, r"Polys\Polys.cas.h5")
-fluent_files = examples.download_fluent_files()
-cas_path = fluent_files["cas"]
-dat_path = fluent_files["dat"]
+# fluent_files = examples.download_fluent_files()
+# cas_path = fluent_files["cas"]
+# dat_path = fluent_files["dat"]
+
 # data_path = r"D:\ANSYSDev\Sandbox\cff\Ans.Dpf.CFF\Ans.Dpf.CFFTest\test_models\CFX"
 # file_path = os.path.join(data_path, r"HeatingCoil\HeatingCoil.res")
+
+data_path = r"D:\ANSYS\DPF\cff\Ans.Dpf.CFF\Ans.Dpf.CFFTest\test_models\fluent\3D"
+cas_path = os.path.join(data_path, r"Polys\Polys.cas.h5")
+dat_path = cas_path
+
 ds = dpf.DataSources()
 ds.set_result_file_path(cas_path, "cas")
 ds.add_file_path(dat_path, "dat")
