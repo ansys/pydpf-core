75032b0c1cbe84854fa682a919c800fc923c7df7
PProfizi <paul.profizi@ansys.com>
2022-10-07 12:56:59 +0200

    Exclude examples/python_plugins from APIDOC as well as examples/examples.py. Add verbose to make html.

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -3,10 +3,11 @@ call sphinx-apidoc -o ../docs/source/api ../ansys ../ansys/dpf/core/log.py ^
 ../ansys/dpf/core/help.py ../ansys/dpf/core/mapping_types.py ../ansys/dpf/core/ipconfig.py ^
 ../ansys/dpf/core/field_base.py ../ansys/dpf/core/cache.py ../ansys/dpf/core/misc.py ^
 ../ansys/dpf/core/check_version.py ../ansys/dpf/core/operators/build.py ../ansys/dpf/core/operators/specification.py ^
-../ansys/dpf/core/vtk_helper.py ../ansys/dpf/core/label_space.py ^
+../ansys/dpf/core/vtk_helper.py ../ansys/dpf/core/label_space.py ../ansys/dpf/core/examples/python_plugins/* ^
+../ansys/dpf/core/examples/examples.py ^
  -f --implicit-namespaces --separate  --no-headings
 pushd .
 cd ../docs/
 call make clean
-call make html
+call make html -v -v -v
 popd
