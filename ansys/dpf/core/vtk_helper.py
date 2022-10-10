7e4169e68cf313695c9186aa39afefe4a3410ba6
paul.profizi <paul.profizi@ansys.com>
2022-10-10 16:18:03 +0200

    WIP

--- ansys/dpf/core/vtk_helper.py
+++ ansys/dpf/core/vtk_helper.py
@@ -1,4 +1,5 @@
 import numpy as np
+import vtkmodules.vtkCommonDataModel
 from vtk import (
     VTK_VERTEX,
     VTK_LINE,
@@ -20,7 +21,11 @@ from vtk import (
     vtkVersion,
 )
 from vtkmodules.vtkCommonCore import (
-    vtkIdList
+    vtkIdList, vtkIdTypeArray,
+)
+from vtkmodules.util.numpy_support import (
+    numpy_to_vtk,
+    numpy_to_vtkIdTypeArray,
 )
 import pyvista as pv
 
@@ -247,23 +252,44 @@ def dpf_mesh_to_vtk(nodes, etypes, connectivity, as_linear=True, mesh=None):
 
     else:
         vtk_cell_type = VTK_MAPPING[etypes]
-
+    vtk_polys_types = VTK_MAPPING[polys_types]
     # different treatment depending on the version of vtk
     if VTK9:
         # compute offset array when < VTK v9
         grid = pv.UnstructuredGrid(cells, vtk_cell_type, nodes)
         # Add polyhedrons
-        for i in polys_indices[0]:
-            faceId = vtkIdList()
-            faceId.InsertNextId(n_faces_per_element[i])  # The number of faces for the element.
-            face_connectivity = elements_faces_connectivity.data[elements_faces_connectivity._data_pointer[i]:elements_faces_connectivity._data_pointer[i+1]]
-            for face_i in range(n_faces_per_element[i]):
-                face_id = face_connectivity[face_i]
-                n_points = n_points_per_face[face_id]
-                face_nodes = faces_nodes_connectivity.data[faces_nodes_connectivity_dp[face_id+face_i-1]:faces_nodes_connectivity_dp[face_id+]]
-                faceId.InsertNextId(len(face_nodes))  # The number of points in the face.
-                [faceId.InsertNextId(i) for i in face_nodes]
-            grid.InsertNextCell(polys_types[i], faceId)
+        for poly_index in polys_indices[0]:
+            faces_vtk = vtkIdList()
+            face_vtkIdList = vtkIdList()
+            n_faces = n_faces_per_element[poly_index]  # The number of faces for the element.
+            face_vtkIdList.InsertNextId(n_faces)
+            face_connectivity = elements_faces_connectivity.data[
+                                elements_faces_connectivity._data_pointer[poly_index]:elements_faces_connectivity._data_pointer[poly_index+1]]
+
+            faces_vtk = []
+            points_vtk = []
+            for face_i in range(n_faces):
+
+                face_index = face_connectivity[face_i]
+                # n_points = n_points_per_face[face_index]
+                face_nodes = faces_nodes_connectivity.data[
+                             faces_nodes_connectivity_dp[face_index]:faces_nodes_connectivity_dp[face_index+1]]
+                face_vtkIdList.InsertNextId(len(face_nodes))  # The number of points in the face.
+                [face_vtkIdList.InsertNextId(i) for i in face_nodes]
+
+                face_as_list = [len(face_nodes)]
+                face_as_list.extend(face_nodes)
+                face_as_array = np.asarray(face_as_list, dtype=np.int64)
+                face_vtk = numpy_to_vtkIdTypeArray(face_as_array)
+                # faces_vtk.InsertNextId(face_vtk.)
+                points_vtk.extend([len(face_nodes)])
+                points_vtk.extend(face_nodes)
+                faces_vtk.extend([face_index])
+
+            ptIds = connectivity.data[connectivity._data_pointer[poly_index]:connectivity._data_pointer[poly_index+1]]
+            # grid.InsertNextCell(polys_types[poly_index], len(ptIds), points_vtk, n_faces, faces_vtk)
+            grid.InsertNextCell(polys_types[poly_index], face_vtkIdList)
+            break
         return grid
 
     # might be computed when checking for VTK quadratic bug
