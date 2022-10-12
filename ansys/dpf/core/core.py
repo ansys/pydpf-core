ace582904a8b17bd42f4a64bd47b9b11129090c1
paul.profizi <paul.profizi@ansys.com>
2022-10-12 14:31:15 +0200

    Debug upload_file_in_tmp_folder

--- ansys/dpf/core/core.py
+++ ansys/dpf/core/core.py
@@ -108,7 +108,7 @@ def upload_file_in_tmp_folder(file_path, new_file_name=None, server=None):
     warnings.warn(f"Uploaded file to {path}")
     if path is None or path == '':
         raise ValueError(f"Upload failed! Path returned is:{path}")
-    return
+    return path
 
 
 def upload_files_in_folder(
