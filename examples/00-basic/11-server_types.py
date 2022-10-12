0c5a2e1e79ea197a54b5faf04454da6b570aee1e
paul.profizi <paul.profizi@ansys.com>
2022-10-12 11:40:44 +0200

    Revert "Comment 11-server_types.py"
    This reverts commit 89d4f75933b67b3643d56888327d6dff8131d9d9.

--- examples/00-basic/11-server_types.py
+++ examples/00-basic/11-server_types.py
@@ -23,67 +23,67 @@ three types of servers are supported:
 """
 
 from ansys.dpf import core as dpf
-#
-# ###############################################################################
-# # Start servers with custom server configuration
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# in_process_config = dpf.AvailableServerConfigs.InProcessServer
-# grpc_config = dpf.AvailableServerConfigs.GrpcServer
-# legacy_grpc_config = dpf.AvailableServerConfigs.LegacyGrpcServer
-#
-# in_process_server = dpf.start_local_server(config=in_process_config)
-# grpc_server = dpf.start_local_server(config=grpc_config)
-# legacy_grpc_server = dpf.start_local_server(config=legacy_grpc_config)
-#
-# ###############################################################################
-# # Equivalent to:
-#
-# in_process_config = dpf.ServerConfig(
-#     protocol=None, legacy=False
-# )
-# grpc_config = dpf.ServerConfig(
-#     protocol=dpf.server_factory.CommunicationProtocols.gRPC, legacy=False
-# )
-# legacy_grpc_config = dpf.ServerConfig(
-#     protocol=dpf.server_factory.CommunicationProtocols.gRPC, legacy=True
-# )
-#
-# in_process_server = dpf.start_local_server(config=in_process_config, as_global=False)
-# grpc_server = dpf.start_local_server(config=grpc_config, as_global=False)
-# legacy_grpc_server = dpf.start_local_server(config=legacy_grpc_config, as_global=False)
-#
-# ###############################################################################
-# # Create data on different servers
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# in_process_field = dpf.fields_factory.create_scalar_field(2, server=in_process_server)
-# in_process_field.append([1.], 1)
-# in_process_field.append([2.], 2)
-# grpc_field = dpf.fields_factory.create_scalar_field(2, server=grpc_server)
-# grpc_field.append([1.], 1)
-# grpc_field.append([2.], 2)
-# legacy_grpc_field = dpf.fields_factory.create_scalar_field(2, server=legacy_grpc_server)
-# legacy_grpc_field.append([1.], 1)
-# legacy_grpc_field.append([2.], 2)
-#
-# print(in_process_field, type(in_process_field._server), in_process_field._server)
-# print(grpc_field, type(grpc_field._server), grpc_field._server)
-# print(legacy_grpc_field, type(legacy_grpc_field._server), legacy_grpc_field._server)
-#
-# ###############################################################################
-# # Choose default configuration
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# # Once a default configuration is chosen, a server of the chosen type is
-# # automatically started when a DPF object is created:
-#
-# initial_config = dpf.SERVER_CONFIGURATION
-#
-# dpf.SERVER_CONFIGURATION = dpf.AvailableServerConfigs.GrpcServer
-# grpc_field = dpf.fields_factory.create_scalar_field(2)
-# grpc_field.append([1.], 1)
-# grpc_field.append([2.], 2)
-# print(grpc_field, type(grpc_field._server), grpc_field._server)
-#
-# # Go back to default config:
-# dpf.SERVER_CONFIGURATION = initial_config
+
+###############################################################################
+# Start servers with custom server configuration
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+in_process_config = dpf.AvailableServerConfigs.InProcessServer
+grpc_config = dpf.AvailableServerConfigs.GrpcServer
+legacy_grpc_config = dpf.AvailableServerConfigs.LegacyGrpcServer
+
+in_process_server = dpf.start_local_server(config=in_process_config)
+grpc_server = dpf.start_local_server(config=grpc_config)
+legacy_grpc_server = dpf.start_local_server(config=legacy_grpc_config)
+
+###############################################################################
+# Equivalent to:
+
+in_process_config = dpf.ServerConfig(
+    protocol=None, legacy=False
+)
+grpc_config = dpf.ServerConfig(
+    protocol=dpf.server_factory.CommunicationProtocols.gRPC, legacy=False
+)
+legacy_grpc_config = dpf.ServerConfig(
+    protocol=dpf.server_factory.CommunicationProtocols.gRPC, legacy=True
+)
+
+in_process_server = dpf.start_local_server(config=in_process_config, as_global=False)
+grpc_server = dpf.start_local_server(config=grpc_config, as_global=False)
+legacy_grpc_server = dpf.start_local_server(config=legacy_grpc_config, as_global=False)
+
+###############################################################################
+# Create data on different servers
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+in_process_field = dpf.fields_factory.create_scalar_field(2, server=in_process_server)
+in_process_field.append([1.], 1)
+in_process_field.append([2.], 2)
+grpc_field = dpf.fields_factory.create_scalar_field(2, server=grpc_server)
+grpc_field.append([1.], 1)
+grpc_field.append([2.], 2)
+legacy_grpc_field = dpf.fields_factory.create_scalar_field(2, server=legacy_grpc_server)
+legacy_grpc_field.append([1.], 1)
+legacy_grpc_field.append([2.], 2)
+
+print(in_process_field, type(in_process_field._server), in_process_field._server)
+print(grpc_field, type(grpc_field._server), grpc_field._server)
+print(legacy_grpc_field, type(legacy_grpc_field._server), legacy_grpc_field._server)
+
+###############################################################################
+# Choose default configuration
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Once a default configuration is chosen, a server of the chosen type is
+# automatically started when a DPF object is created:
+
+initial_config = dpf.SERVER_CONFIGURATION
+
+dpf.SERVER_CONFIGURATION = dpf.AvailableServerConfigs.GrpcServer
+grpc_field = dpf.fields_factory.create_scalar_field(2)
+grpc_field.append([1.], 1)
+grpc_field.append([2.], 2)
+print(grpc_field, type(grpc_field._server), grpc_field._server)
+
+# Go back to default config:
+dpf.SERVER_CONFIGURATION = initial_config
