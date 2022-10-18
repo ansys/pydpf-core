fbec759d694aab8dc6e10f40e1cd13e7b56ca07c
cbellot <camille.bellot@ansys.com>
2022-10-18 13:59:45 +0200

    test animator

--- tests/test_animator.py
+++ tests/test_animator.py
@@ -26,7 +26,7 @@ def remove_gifs(request):
 
 @pytest.fixture()
 def displacement_fields():
-    model = dpf.Model(examples.msup_transient)
+    model = dpf.Model(examples.find_msup_transient())
     mesh_scoping = dpf.mesh_scoping_factory.nodal_scoping(
         model.metadata.meshed_region.nodes.scoping)
     # time_scoping = dpf.time_freq_scoping_factory.scoping_on_all_time_freqs(model)
@@ -107,7 +107,7 @@ def test_animator_animate_fields_container_deform_by_false(remove_gifs, displace
 
 
 def test_animator_animate_fields_container_eqv_partial_scoping(remove_gifs):
-    model = dpf.Model(examples.msup_transient)
+    model = dpf.Model(examples.find_msup_transient())
     time_scoping = dpf.time_freq_scoping_factory.scoping_by_sets(
         list(range(5, 20)))
     stress_result = model.results.stress.on_time_scoping(time_scoping)
@@ -147,7 +147,7 @@ def test_animator_animate_fields_container_scale_factor_raise(displacement_field
 
 
 def test_animator_animate_fields_container_deform_by_result(remove_gifs):
-    model = dpf.Model(examples.msup_transient)
+    model = dpf.Model(examples.find_msup_transient())
     displacement_result = model.results.displacement.on_all_time_freqs
     displacement_fields = displacement_result.eval()
     displacement_fields.animate(deform_by=displacement_result)
@@ -156,7 +156,7 @@ def test_animator_animate_fields_container_deform_by_result(remove_gifs):
 
 
 def test_animator_animate_fields_container_deform_by_operator(remove_gifs):
-    model = dpf.Model(examples.msup_transient)
+    model = dpf.Model(examples.find_msup_transient())
     displacement_op = model.results.displacement.on_all_time_freqs()
     displacement_fields = displacement_op.eval()
     displacement_fields.animate(deform_by=displacement_op)
