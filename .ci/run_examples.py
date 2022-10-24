360956e5b45887e57e189fe852e9b9ec99b80990
paul.profizi <paul.profizi@ansys.com>
2022-10-24 17:53:07 +0200

    debug 02-python_operators_with_dependencies.py

--- .ci/run_examples.py
+++ .ci/run_examples.py
@@ -15,6 +15,8 @@ for root, subdirectories, files in os.walk(os.path.join(actual_path, os.path.par
     for subdirectory in subdirectories:
         subdir = os.path.join(root, subdirectory)
         for file in glob.iglob(os.path.join(subdir, "*.py")):
+            if file != "02-python_operators_with_dependencies.py":
+                continue
             print("\n--------------------------------------------------")
             print(file)
             try:
