6b4b3a2b3d8c26fa3436697a69b7f3723df729e6
paul.profizi <paul.profizi@ansys.com>
2022-11-18 19:04:04 +0100

    Revert "Run only the example of interest"
    This reverts commit f3e3238d022f6ba457924a8ae208876fadcd6151.

--- .ci/run_examples.py
+++ .ci/run_examples.py
@@ -15,8 +15,6 @@ for root, subdirectories, files in os.walk(os.path.join(actual_path, os.path.par
     for subdirectory in subdirectories:
         subdir = os.path.join(root, subdirectory)
         for file in glob.iglob(os.path.join(subdir, "*.py")):
-            if "02-python_operators_with" not in file:
-                continue
             if sys.platform == "linux" and "07-python-operators" in file:
                 continue
             print("\n--------------------------------------------------")
