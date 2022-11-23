c3a77298d4ba7d3ad9f075c90194053b6c1f28f0
guillem.barroso <guillem.barroso@ansys.com>
2022-11-23 15:11:20 +0100

    use __name__ instead of hard-coding name

--- src/ansys/dpf/core/__init__.py
+++ src/ansys/dpf/core/__init__.py
@@ -6,7 +6,7 @@ try:
 except ModuleNotFoundError:
     import importlib_metadata
 
-__version__ = importlib_metadata.version("ansys-dpf-core")
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
 server_to_ansys_grpc_dpf_version = {
     "1.0": "==0.2.2",
