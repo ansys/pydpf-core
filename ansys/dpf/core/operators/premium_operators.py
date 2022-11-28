094df34200c5bd95cf0d8946701683984e3f32cf
paul.profizi <paul.profizi@ansys.com>
2022-11-28 12:08:50 +0100

    Add get_premium_description function in premium_operators.py
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>

--- ansys/dpf/core/operators/premium_operators.py
+++ ansys/dpf/core/operators/premium_operators.py
@@ -3,7 +3,7 @@ from ansys.dpf import core as dpf
 from ansys.dpf.core.dpf_operator import available_operator_names
 
 
-def list_premium_operators(verbose=False):
+def list_premium_operators(verbose: bool = False) -> list:
     # Start a DPF server as entry and a DPF server as premium and query available operators
     server_entry = dpf.start_local_server(context=dpf.AvailableServerContexts.entry,
                                           as_global=False)
@@ -28,5 +28,22 @@ def list_premium_operators(verbose=False):
     return premium_only
 
 
+def get_premium_description(verbose: bool = False) -> str:
+    dpf.set_default_server_context(dpf.AvailableServerContexts.premium)
+    premium_only_operators = list_premium_operators()
+    premium_only_descriptions = {}
+    result = ""
+    if verbose:
+        print(result)
+    for operator_name in premium_only_operators:
+        op = dpf.Operator(operator_name)
+        premium_only_descriptions[operator_name] = op.specification.description
+        lines = f"{operator_name}:\n{op.specification.description}\n"
+        result += lines
+        if verbose:
+            print(lines)
+    return result
+
+
 if __name__ == "__main__":
-    print(list_premium_operators(verbose=True))
+    get_premium_description(verbose=True)
