e524c977fad9f31d5df34c8da9313c22233db9b6
paul.profizi <paul.profizi@ansys.com>
2023-02-06 12:09:23 +0100

    Treat the DPF version note of the examples in the run_examples.py script.

--- .ci/run_examples.py
+++ .ci/run_examples.py
@@ -29,16 +29,16 @@ for root, subdirectories, files in os.walk(os.path.join(actual_path, os.path.par
             # Read the minimal server version required for the example
             version_flag = "This example requires DPF"
             with open(file, "r") as f:
+                minimum_version_str = 0
                 for line in f:
                     if version_flag in line:
-                        minimum_version_str = line.strip(version_flag).split[0]
+                        minimum_version_str = line.strip(version_flag).split()[0]
                         break
-                minimum_version_str = 0
             if float(server_version) - float(minimum_version_str) < -0.05:
-                print(f"Example {f} skipped, requires DPF {minimum_version_str}")
+                print(f"Example skipped as it requires DPF {minimum_version_str}.")
                 continue
             try:
-                subprocess.check_call([sys.executable, file])
+                subprocess.check_output([sys.executable, file])
             except subprocess.CalledProcessError as e:
                 sys.stderr.write(str(e.args))
                 if e.returncode != 3221225477:
