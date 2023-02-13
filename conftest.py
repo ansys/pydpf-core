a4c68b35669143f8d29045580a89833057fb0c80
paul.profizi <paul.profizi@ansys.com>
2023-02-13 13:33:31 +0100

    Fix doctest for features requiring future server version.

--- conftest.py
+++ conftest.py
@@ -3,6 +3,11 @@
 Launch or connect to a persistent local DPF service to be shared in
 pytest as a session fixture
 """
+from doctest import OutputChecker
+from unittest import mock
+
+import pytest
+
 from ansys.dpf import core
 from ansys.dpf.core.misc import module_exists
 
@@ -17,3 +22,17 @@ if module_exists("matplotlib"):
 # enable off_screen plotting to avoid test interruption
 core.settings.disable_off_screen_rendering()
 core.settings.bypass_pv_opengl_osmesa_crash()
+
+
+class DPFOutputChecker(OutputChecker):
+    def check_output(self, want: str, got: str, optionflags: int) -> bool:
+        feature_str = "Feature not supported. Upgrade the server to"
+        if feature_str in got:
+            want = got
+        return OutputChecker.check_output(self, want, got, optionflags)
+
+
+@pytest.fixture(autouse=True)
+def accept_upgrade_error():
+    with mock.patch("doctest.OutputChecker", DPFOutputChecker):
+        yield
