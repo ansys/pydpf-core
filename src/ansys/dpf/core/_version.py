3081f46a2a0af5bf884be92721b0cd4a1cedbfdb
paul.profizi <paul.profizi@ansys.com>
2023-02-23 12:40:28 +0100

    Bump to 0.7.3

--- src/ansys/dpf/core/_version.py
+++ src/ansys/dpf/core/_version.py
@@ -1,6 +1,6 @@
 """Version for ansys-dpf-core"""
 # major, minor, patch
-version_info = 0, 7, 3, "dev0"
+version_info = 0, 7, 3
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
