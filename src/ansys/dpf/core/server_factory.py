955454f9f48bddba43ac8cd194b3fbf8964fbffd
Divya Gupta <divya.gupta@ansys.com>
2023-03-21 09:59:39 +0530

    updated mounted variable

--- src/ansys/dpf/core/server_factory.py
+++ src/ansys/dpf/core/server_factory.py
@@ -166,7 +166,7 @@ class DockerConfig:
             updated_volume_args=""
             for i in  mounted_volumes_args.split(' '):
                 if ':' in i:
-                    i='/'+((i.replace('\\','/')).replace(':', '', 1))
+                    i='/mnt/'+((i.replace('\\','/')).replace(':', '', 1))
                 updated_volume_args=updated_volume_args+" "+i
             mounted_volumes_args=updated_volume_args
 
