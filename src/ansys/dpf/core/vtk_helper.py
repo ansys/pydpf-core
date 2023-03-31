fc4f9719f14a8eceec9b0580bfa81249defd172e
rcanton <rafael.canton@ansys.com>
2023-03-31 08:35:09 +0200

    reduce complexity for Codacy

--- src/ansys/dpf/core/vtk_helper.py
+++ src/ansys/dpf/core/vtk_helper.py
@@ -124,6 +124,170 @@ class PyVistaImportError(ModuleNotFoundError):
         ModuleNotFoundError.__init__(self, msg)
 
 
+def dpf_mesh_to_vtk_op(mesh, nodes, as_linear):
+    """Return a pyvista unstructured grid given DPF node and element
+    definitions from operators (server > 6.2)
+
+    Parameters
+    ----------
+    mesh : dpf.MeshedRegion
+        Meshed Region to export to pyVista format
+
+    nodes : dpf.Field
+        Field containing the nodes of the mesh.
+
+    as_linear : bool
+        Export quadratic surface elements as linear.
+
+    Returns
+    -------
+    grid : pyvista.UnstructuredGrid
+        Unstructured grid of the DPF mesh.
+    """
+    mesh_to_pyvista = dpf.operators.mesh.mesh_to_pyvista()
+    mesh_to_pyvista.inputs.mesh.connect(mesh)
+    mesh_to_pyvista.inputs.as_linear.connect(as_linear)
+    mesh_to_pyvista.inputs.vtk_updated.connect(VTK9)
+    if nodes is not None:
+        mesh_to_pyvista.inputs.coordinates.connect(nodes)
+
+    nodes_pv = mesh_to_pyvista.outputs.nodes()
+    cells_pv = mesh_to_pyvista.outputs.cells()
+    celltypes_pv = mesh_to_pyvista.outputs.cell_types()
+    if VTK9:
+        return pv.UnstructuredGrid(cells_pv, celltypes_pv, nodes_pv)
+    else:
+        offsets_pv = mesh_to_pyvista.outputs.offsets()
+        return pv.UnstructuredGrid(offsets_pv, cells_pv, celltypes_pv, nodes_pv)
+
+
+def dpf_mesh_to_vtk_py(mesh, nodes, as_linear):
+    """Return a pyvista unstructured grid given DPF node and element
+    definitions in pure Python (server <= 6.2)
+
+    Parameters
+    ----------
+    mesh : dpf.MeshedRegion
+        Meshed Region to export to pyVista format
+
+    nodes : dpf.Field
+        Field containing the nodes of the mesh.
+
+    as_linear : bool
+        Export quadratic surface elements as linear.
+
+    Returns
+    -------
+    grid : pyvista.UnstructuredGrid
+        Unstructured grid of the DPF mesh.
+    """
+    etypes = mesh.elements.element_types_field.data
+    connectivity = mesh.elements.connectivities_field
+    if nodes is None:
+        nodes = mesh.nodes.coordinates_field.data
+    else:
+        nodes = nodes.data
+
+    elem_size = np.ediff1d(np.append(connectivity._data_pointer, connectivity.shape))
+
+    faces_nodes_connectivity = mesh.property_field("faces_nodes_connectivity")
+    faces_nodes_connectivity_dp = np.append(
+        faces_nodes_connectivity._data_pointer, len(faces_nodes_connectivity)
+    )
+    elements_faces_connectivity = mesh.property_field("elements_faces_connectivity")
+    elements_faces_connectivity_dp = np.append(
+        elements_faces_connectivity._data_pointer, len(elements_faces_connectivity)
+    )
+
+    insert_ind = np.cumsum(elem_size)
+    insert_ind = np.hstack(([0], insert_ind))[:-1]
+
+    # Handle semiparabolic elements
+    nullmask = connectivity.data == -1
+    connectivity.data[nullmask] = 0
+    if nullmask.any():
+        nodes[0] = np.nan
+
+    # For each polyhedron, cell = [nCellFaces, nFace0pts, i, j, k, ..., nFace1pts, i, j, k, ...]
+    # polys_ind = insert_ind[polyhedron_mask]
+    # cells = np.take(cells, sorted(set(insert_ind)-set(polys_ind)))
+    # partition cells in vtk format
+    cells = np.insert(connectivity.data, insert_ind, elem_size)
+
+    # Check if polyhedrons are present
+    if element_types.Polyhedron.value in etypes:
+        cells = np.array(cells)
+        nodes = np.array(nodes)
+        insert_ind = insert_ind + np.asarray(list(range(len(insert_ind))))
+        # Replace in cells values for polyhedron format
+        # [NValuesToFollow,
+        # NFaces, Face1NPoints, Face1Point1, Face1Point2..., Face1PointN, FaceNNPoints,...]]
+        for i, ind in reversed(list(enumerate(insert_ind))):
+            # Check if this is a polyhedron
+            if etypes[i] == element_types.Polyhedron.value:
+                # Construct the connectivity for the poly element
+                poly_connectivity = []
+                faces = elements_faces_connectivity.data[
+                    elements_faces_connectivity_dp[i] : elements_faces_connectivity_dp[i + 1]
+                ]
+                for face in faces:
+                    face_connectivity = faces_nodes_connectivity.data[
+                        faces_nodes_connectivity_dp[face] : faces_nodes_connectivity_dp[face + 1]
+                    ]
+                    face_fmt = [len(face_connectivity)] + list(face_connectivity)
+                    poly_connectivity += face_fmt
+                polyhedron = [len(faces)] + poly_connectivity
+                polyhedron = [len(polyhedron)] + polyhedron
+                # Replace the whole sequence between this index and the next
+                r = list(range(ind, ind + elem_size[i] + 1))
+                cells = np.delete(cells, r)
+                cells = np.insert(cells, ind, polyhedron)
+
+    def compute_offset():
+        """Return the starting point of a cell in the cells array"""
+        return insert_ind + np.arange(insert_ind.size)
+
+    # convert kAns to VTK cell type
+    offset = None
+    if as_linear:
+        vtk_cell_type = VTK_LINEAR_MAPPING[etypes]
+
+        # visualization bug within VTK with quadratic surf cells
+        ansquad8_mask = etypes == 6
+        if np.any(ansquad8_mask):  # kAnsQuad8
+
+            # simply copy the edge node indices to the midside points
+            offset = compute_offset()
+            cell_pos = offset[ansquad8_mask]
+            cells[cell_pos + 5] = cells[cell_pos + 1]
+            cells[cell_pos + 6] = cells[cell_pos + 2]
+            cells[cell_pos + 7] = cells[cell_pos + 3]
+            cells[cell_pos + 8] = cells[cell_pos + 4]
+
+        anstri6_mask = etypes == 4  # kAnsTri6 = 4
+        if np.any(anstri6_mask):
+            if offset is None:
+                offset = compute_offset()
+            cell_pos = offset[anstri6_mask]
+            cells[cell_pos + 4] = cells[cell_pos + 1]
+            cells[cell_pos + 5] = cells[cell_pos + 2]
+            cells[cell_pos + 6] = cells[cell_pos + 3]
+
+    else:
+        vtk_cell_type = VTK_MAPPING[etypes]
+
+    # different treatment depending on the version of vtk
+    if VTK9:
+        # compute offset array when < VTK v9
+        return pv.UnstructuredGrid(cells, vtk_cell_type, nodes)
+
+    # might be computed when checking for VTK quadratic bug
+    if offset is None:
+        offset = compute_offset()
+
+    return pv.UnstructuredGrid(offset, cells, vtk_cell_type, nodes)
+
+
 def dpf_mesh_to_vtk(mesh, nodes=None, as_linear=True):
     """Return a pyvista unstructured grid given DPF node and element
     definitions.
@@ -145,133 +309,9 @@ def dpf_mesh_to_vtk(mesh, nodes=None, as_linear=True):
         Unstructured grid of the DPF mesh.
     """
     try:
-        mesh_to_pyvista = dpf.operators.mesh.mesh_to_pyvista()
-        mesh_to_pyvista.inputs.mesh.connect(mesh)
-        mesh_to_pyvista.inputs.as_linear.connect(as_linear)
-        mesh_to_pyvista.inputs.vtk_updated.connect(VTK9)
-        if nodes is not None:
-            mesh_to_pyvista.inputs.coordinates.connect(nodes)
-
-        nodes_pv = mesh_to_pyvista.outputs.nodes()
-        cells_pv = mesh_to_pyvista.outputs.cells()
-        celltypes_pv = mesh_to_pyvista.outputs.cell_types()
-        if VTK9:
-            return pv.UnstructuredGrid(cells_pv, celltypes_pv, nodes_pv)
-        else:
-            offsets_pv = mesh_to_pyvista.outputs.offsets()
-            return pv.UnstructuredGrid(offsets_pv, cells_pv, celltypes_pv, nodes_pv)
-
-    except (AttributeError, KeyError, errors.DPFServerException) as e:
-
-        # If the operator is not available (past server versions)
-
-        etypes = mesh.elements.element_types_field.data
-        connectivity = mesh.elements.connectivities_field
-        if nodes is None:
-            nodes = mesh.nodes.coordinates_field.data
-        else:
-            nodes = nodes.data
-
-        elem_size = np.ediff1d(np.append(connectivity._data_pointer, connectivity.shape))
-
-        faces_nodes_connectivity = mesh.property_field("faces_nodes_connectivity")
-        faces_nodes_connectivity_dp = np.append(
-            faces_nodes_connectivity._data_pointer, len(faces_nodes_connectivity)
-        )
-        elements_faces_connectivity = mesh.property_field("elements_faces_connectivity")
-        elements_faces_connectivity_dp = np.append(
-            elements_faces_connectivity._data_pointer, len(elements_faces_connectivity)
-        )
-
-        insert_ind = np.cumsum(elem_size)
-        insert_ind = np.hstack(([0], insert_ind))[:-1]
-
-        # Handle semiparabolic elements
-        nullmask = connectivity.data == -1
-        connectivity.data[nullmask] = 0
-        if nullmask.any():
-            nodes[0] = np.nan
-
-        # For each polyhedron, cell = [nCellFaces, nFace0pts, i, j, k, ..., nFace1pts, i, j, k, ...]
-        # polys_ind = insert_ind[polyhedron_mask]
-        # cells = np.take(cells, sorted(set(insert_ind)-set(polys_ind)))
-        # partition cells in vtk format
-        cells = np.insert(connectivity.data, insert_ind, elem_size)
-
-        # Check if polyhedrons are present
-        if element_types.Polyhedron.value in etypes:
-            cells = np.array(cells)
-            nodes = np.array(nodes)
-            insert_ind = insert_ind + np.asarray(list(range(len(insert_ind))))
-            # Replace in cells values for polyhedron format
-            # [NValuesToFollow,
-            # NFaces, Face1NPoints, Face1Point1, Face1Point2..., Face1PointN, FaceNNPoints,...]]
-            for i, ind in reversed(list(enumerate(insert_ind))):
-                # Check if this is a polyhedron
-                if etypes[i] == element_types.Polyhedron.value:
-                    # Construct the connectivity for the poly element
-                    poly_connectivity = []
-                    faces = elements_faces_connectivity.data[
-                        elements_faces_connectivity_dp[i] : elements_faces_connectivity_dp[i + 1]
-                    ]
-                    for face in faces:
-                        face_connectivity = faces_nodes_connectivity.data[
-                            faces_nodes_connectivity_dp[face] : faces_nodes_connectivity_dp[
-                                face + 1
-                            ]
-                        ]
-                        face_fmt = [len(face_connectivity)] + list(face_connectivity)
-                        poly_connectivity += face_fmt
-                    polyhedron = [len(faces)] + poly_connectivity
-                    polyhedron = [len(polyhedron)] + polyhedron
-                    # Replace the whole sequence between this index and the next
-                    r = list(range(ind, ind + elem_size[i] + 1))
-                    cells = np.delete(cells, r)
-                    cells = np.insert(cells, ind, polyhedron)
-
-        def compute_offset():
-            """Return the starting point of a cell in the cells array"""
-            return insert_ind + np.arange(insert_ind.size)
-
-        # convert kAns to VTK cell type
-        offset = None
-        if as_linear:
-            vtk_cell_type = VTK_LINEAR_MAPPING[etypes]
-
-            # visualization bug within VTK with quadratic surf cells
-            ansquad8_mask = etypes == 6
-            if np.any(ansquad8_mask):  # kAnsQuad8
-
-                # simply copy the edge node indices to the midside points
-                offset = compute_offset()
-                cell_pos = offset[ansquad8_mask]
-                cells[cell_pos + 5] = cells[cell_pos + 1]
-                cells[cell_pos + 6] = cells[cell_pos + 2]
-                cells[cell_pos + 7] = cells[cell_pos + 3]
-                cells[cell_pos + 8] = cells[cell_pos + 4]
-
-            anstri6_mask = etypes == 4  # kAnsTri6 = 4
-            if np.any(anstri6_mask):
-                if offset is None:
-                    offset = compute_offset()
-                cell_pos = offset[anstri6_mask]
-                cells[cell_pos + 4] = cells[cell_pos + 1]
-                cells[cell_pos + 5] = cells[cell_pos + 2]
-                cells[cell_pos + 6] = cells[cell_pos + 3]
-
-        else:
-            vtk_cell_type = VTK_MAPPING[etypes]
-
-        # different treatment depending on the version of vtk
-        if VTK9:
-            # compute offset array when < VTK v9
-            return pv.UnstructuredGrid(cells, vtk_cell_type, nodes)
-
-        # might be computed when checking for VTK quadratic bug
-        if offset is None:
-            offset = compute_offset()
-
-        return pv.UnstructuredGrid(offset, cells, vtk_cell_type, nodes)
+        return dpf_mesh_to_vtk_op(mesh, nodes, as_linear)
+    except (AttributeError, KeyError, errors.DPFServerException):
+        return dpf_mesh_to_vtk_py(mesh, nodes, as_linear)
 
 
 def vtk_update_coordinates(vtk_grid, coordinates_array):
