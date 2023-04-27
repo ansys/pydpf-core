ce65369cb07f953b33e1ab3425f0283f35fbfb82
paul.profizi <paul.profizi@ansys.com>
2023-04-27 11:49:08 +0200

    Plot the field alone

--- examples/09-averaging/00-compute_and_average.py
+++ examples/09-averaging/00-compute_and_average.py
@@ -99,8 +99,8 @@ def compute_von_mises_then_average(analysis):
     min_max.inputs.field.connect(avg_von_mises)
     max_val = min_max.outputs.field_max()
 
-    _ = mesh.grid
-    # mesh.plot()
+    avg_von_mises.plot()
+    # mesh.plot(avg_von_mises)
 
     return max_val.data[0]
 
