607f1b806e65a7af3c49000d4ec46ca67a75e6a0
lparadis <lea.paradis@ansys.com>
2023-05-30 16:33:58 +0200

    Update

--- src/ansys/dpf/core/dpf_operator.py
+++ src/ansys/dpf/core/dpf_operator.py
@@ -485,7 +485,8 @@ class Operator:
 
         Examples
         --------
-        Modify the copy of an operator's configuration and set it back.
+        Modify the copy of an operator's configuration and set it as current config
+        for the operator.
 
         >>> from ansys.dpf import core as dpf
         >>> op = dpf.operators.math.add()
