62b2e918b1fd2263702a10e4dce4858aa385c269
paul.profizi <paul.profizi@ansys.com>
2023-06-01 14:32:30 +0200

    Revert "Check bug for Docstring Tests on Linux for core.download_file"
    This reverts commit a5e35b81c304b9397771d0cf7d5d255115695aa5.

--- src/ansys/dpf/core/core.py
+++ src/ansys/dpf/core/core.py
@@ -168,6 +168,7 @@ def download_file(server_file_path, to_client_file_path, server=None):
     >>> from ansys.dpf.core import examples
     >>> server = dpf.start_local_server(config=dpf.AvailableServerConfigs.GrpcServer,
     ... as_global=False)
+    >>> file_path = dpf.upload_file_in_tmp_folder(examples.find_static_rst(), server=server)
 
     Notes
     -----
