0cc7918b9f707906050b41ed1e037d2b55954538
paul.profizi <paul.profizi@ansys.com>
2023-06-02 16:45:52 +0200

    REmove problematic docstring example for core.download_file

--- src/ansys/dpf/core/core.py
+++ src/ansys/dpf/core/core.py
@@ -162,17 +162,6 @@ def download_file(server_file_path, to_client_file_path, server=None):
         Server with channel connected to the remote or local instance. When
         ``None``, attempts to use the global server.
 
-    Examples
-    --------
-    >>> from ansys.dpf import core as dpf
-    >>> from ansys.dpf.core import examples
-    >>> server = dpf.start_local_server(config=dpf.AvailableServerConfigs.GrpcServer,
-    ... as_global=False)
-    >>> file_path = dpf.upload_file_in_tmp_folder(examples.find_static_rst(), server=server)
-    >>> dpf.download_file(file_path, examples.find_static_rst(),  server=server)
-    <BLANKLINE>
-    ...
-
     Notes
     -----
     Is not implemented for usage with type(server)=
