ce34e514634cbf391630f43c921eb1c3f57bebdf
Antoine Karcher <antoine.karcher@ansys.com>
2023-06-20 16:40:34 +0200

    Skip incremental related tests if before 23R2

--- tests/test_incremental.py
+++ tests/test_incremental.py
@@ -1,8 +1,14 @@
 from ansys.dpf import core
 from ansys.dpf.core import common
 import numpy as np
+import pytest
+import conftest
 
 
+@pytest.mark.skipif(
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_6_0,
+    reason="for_each not implemented below 6.0",
+)
 def test_incremental_results(server_type, plate_msup):
     ds = core.DataSources(plate_msup, server=server_type)
     scoping = core.time_freq_scoping_factory.scoping_on_all_time_freqs(ds)
@@ -32,6 +38,10 @@ def test_incremental_results(server_type, plate_msup):
         assert np.isclose(ref_field.data, inc_field.data).all()
 
 
+@pytest.mark.skipif(
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_6_0,
+    reason="for_each not implemented below 6.0",
+)
 def test_incremental_minmax(server_type, plate_msup):
     ds = core.DataSources(plate_msup, server=server_type)
     scoping = core.time_freq_scoping_factory.scoping_on_all_time_freqs(ds)
@@ -62,6 +72,10 @@ def test_incremental_minmax(server_type, plate_msup):
     assert np.isclose(ref_field_max.data, inc_field_max.data).all()
 
 
+@pytest.mark.skipif(
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_6_0,
+    reason="for_each not implemented below 6.0",
+)
 def test_incremental_accumulate(server_type, plate_msup):
     ds = core.DataSources(plate_msup, server=server_type)
     scoping = core.time_freq_scoping_factory.scoping_on_all_time_freqs(ds)
@@ -89,6 +103,10 @@ def test_incremental_accumulate(server_type, plate_msup):
     assert np.isclose(ref_field.data, inc_field.data).all()
 
 
+@pytest.mark.skipif(
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_6_0,
+    reason="for_each not implemented below 6.0",
+)
 def test_incremental_average(server_type, plate_msup):
     ds = core.DataSources(plate_msup, server=server_type)
     scoping = core.time_freq_scoping_factory.scoping_on_all_time_freqs(ds)
