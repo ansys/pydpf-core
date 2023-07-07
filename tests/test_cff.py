8e2caa6359f9675440ee96d1b3cdccc6247a64f5
lparadis <lea.paradis@ansys.com>
2023-07-07 14:49:47 +0200

    Revert "Reset fixture"
    This reverts commit 1a12e69c3633ae45b33da12e405609652920308b.

--- tests/test_cff.py
+++ tests/test_cff.py
@@ -2,15 +2,6 @@ import pytest
 import conftest
 import platform
 from ansys.dpf import core as dpf
-from ansys.dpf.core import examples
-
-
-def fluent_multi_species_on_server(server):
-    ds = dpf.DataSources(server=server)
-    files = examples.download_fluent_multi_species(server=server)
-    ds.set_result_file_path(files["cas"], "cas")
-    ds.add_file_path(files["dat"], "dat")
-    return ds
 
 
 @pytest.mark.skipif(
@@ -18,7 +9,7 @@ def fluent_multi_species_on_server(server):
     reason="CFF source operators where not supported before 7.0,",
 )
 def test_cff_model_in_process(server_in_process, fluent_multi_species):
-    ds = fluent_multi_species_on_server(server_in_process)
+    ds = fluent_multi_species(server_in_process)
     model = dpf.Model(ds, server=server_in_process)
     print(model)
     assert model is not None
@@ -29,7 +20,7 @@ def test_cff_model_in_process(server_in_process, fluent_multi_species):
     reason="CFF source operators where not supported before 7.0,",
 )
 def test_cff_model_grpc_servers(server_type_remote_process, fluent_multi_species):
-    ds = fluent_multi_species_on_server(server_type_remote_process)
+    ds = fluent_multi_species(server_type_remote_process)
     model = dpf.Model(ds, server=server_type_remote_process)
     print(model)
     assert model is not None
