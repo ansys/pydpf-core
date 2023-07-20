357f9bd0896a8339a12fb5fd9bdfbb4f7fdbdaf3
lparadis <lea.paradis@ansys.com>
2023-07-20 16:29:00 +0200

    enhance descr

--- src/ansys/dpf/core/helpers.py
+++ src/ansys/dpf/core/helpers.py
@@ -5,16 +5,21 @@ streamlines.
 """
 
 def compute_streamlines(meshed_region, field, **kwargs):
-    """
+    """Compute the streamlines for a given mesh and velocity
+    field.
 
     Parameters
     ----------
-    meshed_region
-    field
-    kwargs
+    meshed_region: MeshedRegion
+    field: Field
+    **kwargs : optional
+        Additional keyword arguments for the streamline
+        computation. More information is available at
+        :func:`pyvista.DataSetFilters.streamlines`.
 
     Returns
     -------
+    streamlines: FieldsContainer
 
     """
     pass
