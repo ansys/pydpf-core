8beb8e59e1f67a97c0a6b61911ffcc4ae9b52183
lparadis <lea.paradis@ansys.com>
2023-07-20 16:16:18 +0200

    Add helpers

---
+++ src/ansys/dpf/core/helpers.py
@@ -0,0 +1,20 @@
+"""Helpers for using specific capabilities.
+
+For example, provide helpers to compute and manipulate
+streamlines.
+"""
+
+def compute_streamlines(meshed_region, field, **kwargs):
+    """
+
+    Parameters
+    ----------
+    meshed_region
+    field
+    kwargs
+
+    Returns
+    -------
+
+    """
+    pass
