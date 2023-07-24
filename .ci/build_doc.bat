ce9774cb61c7fdbaeb2557fcb39ed37b9c913404
rcanton <rafael.canton@ansys.com>
2023-07-24 12:36:24 +0200

    right order of inputs

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -15,12 +15,12 @@ dir
 
 rem Patch pyVista issue with elemental plots
 
-xcopy /y source/examples/04-advanced/02-volume_averaged_stress/sphx_glr_02-volume_averaged_stress_001.png   build/html/_images/sphx_glr_02-volume_averaged_stress_001.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_001.png                     build/html/_images/sphx_glr_02-fluids_results_001.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_002.png                     build/html/_images/sphx_glr_02-fluids_results_002.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_003.png                     build/html/_images/sphx_glr_02-fluids_results_003.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_004.png                     build/html/_images/sphx_glr_02-fluids_results_004.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_005.png                     build/html/_images/sphx_glr_02-fluids_results_005.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_006.png                     build/html/_images/sphx_glr_02-fluids_results_006.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_007.png                     build/html/_images/sphx_glr_02-fluids_results_007.png
-xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_00_thumb.png                build/html/_images/sphx_glr_02-fluids_results_00_thumb.png
+xcopy  source/examples/04-advanced/02-volume_averaged_stress/sphx_glr_02-volume_averaged_stress_001.png   build/html/_images/sphx_glr_02-volume_averaged_stress_001.png  /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_001.png                     build/html/_images/sphx_glr_02-fluids_results_001.png          /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_002.png                     build/html/_images/sphx_glr_02-fluids_results_002.png          /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_003.png                     build/html/_images/sphx_glr_02-fluids_results_003.png          /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_004.png                     build/html/_images/sphx_glr_02-fluids_results_004.png          /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_005.png                     build/html/_images/sphx_glr_02-fluids_results_005.png          /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_006.png                     build/html/_images/sphx_glr_02-fluids_results_006.png          /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_007.png                     build/html/_images/sphx_glr_02-fluids_results_007.png          /y
+xcopy  source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_00_thumb.png                build/html/_images/sphx_glr_02-fluids_results_00_thumb.png     /y
