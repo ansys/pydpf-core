e8d8579af1bac47ff9fdf03d87771103c5c86cea
Michael Nale <michael.nale@ansys.com>
2023-07-24 10:28:30 +0200

    Create examples for isosurfaces using fluid files

---
+++ examples/12-fluids/03-fluids_isosurface.py
@@ -0,0 +1,10 @@
+"""
+.. _ref_fluids_isosurface:
+
+Compute iso-surfaces on fluid models
+------------------------------------------------------
+
+This example shows how to compute iso-surfaces on fluid models with and without
+polygons and polyhedrons.
+"""
+
