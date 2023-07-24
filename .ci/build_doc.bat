1defc3c356a9d50b0eec5f29b5ac8ed538adcade
rcanton <rafael.canton@ansys.com>
2023-07-24 11:50:29 +0200

    patch pyvista issue

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -10,4 +10,17 @@ pushd .
 cd ../docs/
 call make clean
 call make html -v -v -v -P
-popd
+
+dir
+
+rem Patch pyVista issue with elemental plots
+
+xcopy /y source/examples/04-advanced/02-volume_averaged_stress/sphx_glr_02-volume_averaged_stress_001.png   build/html/_images/sphx_glr_02-volume_averaged_stress_001.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_001.png                     build/html/_images/sphx_glr_02-fluids_results_001.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_002.png                     build/html/_images/sphx_glr_02-fluids_results_002.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_003.png                     build/html/_images/sphx_glr_02-fluids_results_003.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_004.png                     build/html/_images/sphx_glr_02-fluids_results_004.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_005.png                     build/html/_images/sphx_glr_02-fluids_results_005.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_006.png                     build/html/_images/sphx_glr_02-fluids_results_006.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_007.png                     build/html/_images/sphx_glr_02-fluids_results_007.png
+xcopy /y source/examples/12-fluids/02-fluids_results/sphx_glr_02-fluids_results_00_thumb.png                build/html/_images/sphx_glr_02-fluids_results_00_thumb.png
