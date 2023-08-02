520a8212cef03ff22fa3e6b57de032c7754d414f
lparadis <lea.paradis@ansys.com>
2023-08-02 10:05:47 +0200

    Update doc for instantiating operator

--- src/ansys/dpf/core/dpf_operator.py
+++ src/ansys/dpf/core/dpf_operator.py
@@ -120,7 +120,7 @@ class Operator:
             raise KeyError(
                 f"The operator {self.name} doesn't exist in the registry. "
                 f"Check its spelling in the documentation or verify its availability "
-                f"in your Context (Entry/Premium) and in your loaded plugins."
+                f"in your loaded plugins."
             )
 
         self._spec = Specification(operator_name=self.name, server=self._server)
