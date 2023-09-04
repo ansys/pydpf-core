7bfd2ebf2927e04a4a62fabe87bcf32cc9f33987
paul.profizi <paul.profizi@ansys.com>
2023-09-04 15:26:01 +0200

    Clean-up examples.py

--- src/ansys/dpf/core/examples/examples.py
+++ src/ansys/dpf/core/examples/examples.py
@@ -8,33 +8,10 @@ Examples result files.
 
 import os
 
-# import inspect
-
 from ansys.dpf.core import server as server_module
 from ansys.dpf.core.core import upload_file_in_tmp_folder
 from ansys.dpf.core import path_utilities
 from ansys.dpf.core import DataSources
-
-# if os.environ.get("DPF_DOCKER", "").lower() == "true":
-#     # must pass a path that can be accessed by a docker image with
-#     # this directory mounted at the repository level for CI
-#     _module_path = r"/dpf/ansys/dpf/core/examples/result_files/"
-# else:
-#     _module_path = os.path.join(
-#         os.path.dirname(inspect.getfile(inspect.currentframe())), "result_files"
-#     )
-#
-# # this files can be imported with from `ansys.dpf.core import examples`:
-# simple_bar = os.path.join(_module_path, "ASimpleBar.rst")
-# static_rst = os.path.join(_module_path, "static.rst")
-# complex_rst = os.path.join(_module_path, "complex.rst")
-# multishells_rst = os.path.join(_module_path, "model_with_ns.rst")
-# electric_therm = os.path.join(_module_path, "rth", "rth_electric.rth")
-# steady_therm = os.path.join(_module_path, "rth", "rth_steady.rth")
-# transient_therm = os.path.join(_module_path, "rth", "rth_transient.rth")
-# msup_transient = os.path.join(_module_path, "msup_transient_plate1.rst")
-# simple_cyclic = os.path.join(_module_path, "file_cyclic.rst")
-# distributed_msup_folder = os.path.join(_module_path, "msup_distributed")
 
 
 def get_example_required_minimum_dpf_version(file: os.PathLike) -> str:
