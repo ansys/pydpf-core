2f23846200c1d153343a4f1f6cd86d2681fd5a00
paul.profizi <paul.profizi@ansys.com>
2023-09-19 15:23:38 +0200

    Temp save of setup.py

--- setup.py
+++ setup.py
@@ -4,36 +4,40 @@
 # to allow -e with pip<21.1
 from setuptools import setup, find_namespace_packages
 
+# gatebin_binaries = ["*.so"]
+
+
+# def set_gatebin(plat):
+#     if plat == "toto":
+#         global gatebin_binaries
+#         gatebin_binaries = []
+#
+#
+#
+# try:
+#     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+#
+#
+#     class bdist_wheel(_bdist_wheel):
+#         def get_tag(self):
+#             python, abi, plat = _bdist_wheel.get_tag(self)
+#             # set_gatebin(plat)
+#             return python, abi, plat
+# except ImportError:
+#     bdist_wheel = None
+
 
 setup(
-    package_dir={"": "src"},
-    include_package_data=True,
-    packages=find_namespace_packages(where="src"),
-    package_data={
-        "ansys.dpf.gatebin": ["*.so", "*.dll"],
-        "ansys.dpf.core.examples": ["**/*"],
-    },
+    # package_dir={"": "src"},
+    # include_package_data=True,
+    # packages=find_namespace_packages(where="src"),
+    # package_data={
+    #     # "ansys.dpf.gatebin": gatebin_binaries,
+    #     "ansys.dpf.core.examples": ["**/*"],
+    # },
+    # exclude_package_data={
+    #     "ansys.dpf.gatebin": ["**/*.so"],
+    #     "ansys.dpf.core.examples": ["**/*.lck"]
+    # }
 )
-# "ansys.dpf.core.examples" = [
-#     "ASimpleBar.rst",
-#     "static.rst",
-#     "complex.rst",
-#     "model_with_ns.rst",
-#     "file_cyclic.rst",
-#     "msup_transient_plate1.rst",
-#     "rth/rth_electric.rth",
-#     "rth/rth_steady.rth",
-#     "rth/rth_transient.rth",
-#     "sub/cp56.sub",
-#     "msup/file.mode",
-#     "msup/file.rst",
-#     "msup/file.rfrq",
-#     "distributed/file0.rst",
-#     "distributed/file1.rst",
-#     "msup_distributed/file0.rst",
-#     "msup_distributed/file1.rst",
-#     "msup_distributed/file0.mode",
-#     "msup_distributed/file1.mode",
-#     "msup_distributed/file_load_1.rfrq",
-#     "msup_distributed/file_load_2.rfrq",
-# ]
+
