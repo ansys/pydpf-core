01da8c1bed9cd9adebacf64e2c6c43325bddb9e4
paul.profizi <paul.profizi@ansys.com>
2023-09-19 17:44:35 +0200

    Try setup.cfg with setup.py

--- setup.py
+++ setup.py
@@ -2,42 +2,7 @@
 """
 # To keep according to https://setuptools.pypa.io/en/stable/userguide/pyproject_config.html
 # to allow -e with pip<21.1
-from setuptools import setup, find_namespace_packages
-
-# gatebin_binaries = ["*.so"]
+from setuptools import setup
 
 
-# def set_gatebin(plat):
-#     if plat == "toto":
-#         global gatebin_binaries
-#         gatebin_binaries = []
-#
-#
-#
-# try:
-#     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-#
-#
-#     class bdist_wheel(_bdist_wheel):
-#         def get_tag(self):
-#             python, abi, plat = _bdist_wheel.get_tag(self)
-#             # set_gatebin(plat)
-#             return python, abi, plat
-# except ImportError:
-#     bdist_wheel = None
-
-
-setup(
-    # package_dir={"": "src"},
-    # include_package_data=True,
-    # packages=find_namespace_packages(where="src"),
-    # package_data={
-    #     # "ansys.dpf.gatebin": gatebin_binaries,
-    #     "ansys.dpf.core.examples": ["**/*"],
-    # },
-    # exclude_package_data={
-    #     "ansys.dpf.gatebin": ["**/*.so"],
-    #     "ansys.dpf.core.examples": ["**/*.lck"]
-    # }
-)
-
+setup()
