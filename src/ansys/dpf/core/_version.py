e478ea6c8b2a902d03e617050d03f47235ef7a76
paul.profizi <paul.profizi@ansys.com>
2023-10-12 18:54:36 +0200

    Refactor server_to_ansys_version

--- src/ansys/dpf/core/_version.py
+++ src/ansys/dpf/core/_version.py
@@ -4,7 +4,7 @@ version_info = 0, 9, 1, "dev0"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
-min_server_version = "2.0"
+min_server_version = "4.0"
 
 server_to_ansys_grpc_dpf_version = {
     "1.0": "==0.2.2",
@@ -16,15 +16,27 @@ server_to_ansys_grpc_dpf_version = {
     "7.0": ">=0.8dev",
 }
 
-server_to_ansys_version = {
-    "1.0": "2021R1",
-    "2.0": "2021R2",
-    "3.0": "2022R1",
-    "4.0": "2022R2",
-    "5.0": "2023R1",
-    "6.0": "2023R2",
-    "6.1": "2023R2",
-    "6.2": "2023R2",
-    "7.0": "2024R1",
-    "7.1": "2024R1",
-}
+
+class ServerToAnsysVersion:
+    legacy_version_map = {
+        "1.0": "2021R1",
+        "2.0": "2021R2",
+        "3.0": "2022R1",
+        "4.0": "2022R2",
+        "5.0": "2023R1",
+        "6.0": "2023R2",
+        "6.1": "2023R2",
+        "6.2": "2023R2",
+        "7.0": "2024R1",
+        "7.1": "2024R1",
+    }
+
+    def __getitem__(self, item):
+        if len(item) == 3:
+            return self.legacy_version_map[item]
+        else:
+            split = item.split('.')
+            return split[0]+'R'+split[1]
+
+
+server_to_ansys_version = ServerToAnsysVersion()
