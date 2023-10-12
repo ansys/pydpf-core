8e1a27112a44fff35e2ef1f5a0db4b9670217f76
Paul Profizi <100710998+PProfizi@users.noreply.github.com>
2023-10-12 14:54:02 +0200

    Produce OS-specific wheels  (#1166)
    * Update releaser.yml to get and publish all .whl files from the Release assets to PyPI, as well as the .tar.gz and .zip sources
    
    * Temp save of setup.py
    
    * Delete setup.py and declare everything in pyproject.toml
    
    * Draft version of setup.cfg
    
    * Revert "Delete setup.py and declare everything in pyproject.toml"
    
    This reverts commit f1dbb61837feaa7fc20955e75c9731043b735bdc.
    
    * Try setup.cfg with setup.py
    
    * Add wheel as build requirement in pyproject.toml
    
    * Try build jobs in the CI
    
    * Only run build jobs
    
    * Add build to requirements_build.txt
    
    * Update ci.yml build wheel job
    
    * Update ci.yml build wheel job
    
    * Create .ci/build_wheel.py script to create platform-specific wheels
    
    * Update ci.yml to use .ci/build_wheel.py
    
    * Add --wheelhouse argument to .ci/build_wheel.py, switch to pip wheel
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.Test build wheel during test
    
    * Skip build_package action in test.yml
    
    * Fix path to wheel
    
    * Fix optional dependencies
    
    * Upload wheel and test docstrings
    
    * Switch back to build when not building a wheelhouse
    
    * Test upload of wheelhouse
    
    * Do not add plotting optional dependencies when building the wheelhouse
    
    * Update wheelhouse name
    
    * Update wheelhouse name
    
    * Update wheelhouse name
    
    * Set back wheelhouse arg to false in ci.yml/tests
    
    * Activate tests back in tests.yml
    
    * Tests all tests in ci.yml
    
    * Update default python version in tests.yml
    
    * Update examples.yml
    
    * Update pydpf-post.yml
    
    * Update docs.yml
    
    * Update test_docker.yml
    
    * Update examples_docker.yml
    
    * Update examples_docker.yml
    
    * Update gate.yml
    
    * Update ci_release.yml
    
    * Add tests_any job
    
    * Add tests_any job
    
    * Update build_wheel.py to also remove the gatebin folder when any
    
    * Update build_wheel.py to also remove the gatebin folder when any
    
    * Add timeout to tests.yml
    
    * Remove build_any job from ci.yml
    
    * Add tests_any job to ci_release.yml
    
    * Fix test_load_api_without_awp_root_no_gatebin
    
    * Fix test_load_api_without_awp_root_no_gatebin
    
    * Update test_service.py/test_load_api_without_awp_root_no_gatebin
    
    * Remove gate.yml

---
+++ .ci/build_wheel.py
@@ -0,0 +1,94 @@
+# This script generates the different versions of the ansys-dpf-core wheels based on a given input.
+# Input can be one of ["any", "win", "manylinux1", "manylinux_2_17"]
+
+import argparse
+import pathlib
+import subprocess
+import os
+import sys
+import shutil
+import tempfile
+
+
+supported_platforms = {
+    "any": "any",
+    "win": "win_amd64",
+    "manylinux1": "manylinux1_x86_64",
+    "manylinux_2_17": "manylinux_2_17_x86_64"
+}
+
+argParser = argparse.ArgumentParser()
+argParser.add_argument("-p", "--platform", help="platform")
+argParser.add_argument("-w", "--wheelhouse", help="platform", action='store_true')
+
+args = argParser.parse_args()
+
+if args.platform not in supported_platforms:
+    raise ValueError(f"Platform {args.platform} is not supported. "
+                     f"Supported platforms are: {list(supported_platforms.keys())}")
+else:
+    requested_platform = supported_platforms[args.platform]
+print(requested_platform)
+
+# Move binaries out of the source depending on platform requested
+# any: move all binaries out before building
+# win: move .so binaries out before building
+# lin: move .dll binaries out before building
+with tempfile.TemporaryDirectory() as tmpdirname:
+    print('Created temporary directory: ', tmpdirname)
+
+    # Create the temporary build-opts.cfg
+    build_opts_path = os.path.join(tmpdirname, "build-opts.cfg")
+    with open(build_opts_path, "w") as build_opts_file:
+        build_opts_file.write(f"[bdist_wheel]\nplat-name={requested_platform}")
+    os.environ["DIST_EXTRA_CONFIG"] = build_opts_path
+
+    # Move the binaries
+    gatebin_folder_path = os.path.join(
+        os.path.curdir,
+        os.path.join("src", "ansys", "dpf", "gatebin")
+    )
+    binaries_to_move = []
+    moved = []
+    if "win" in requested_platform or "any" == requested_platform:
+        # Move linux binaries
+        binaries_to_move.extend(["libAns.Dpf.GrpcClient.so", "libDPFClientAPI.so"])
+    if "linux" in requested_platform or "any" == requested_platform:
+        # Move windows binaries
+        binaries_to_move.extend(["Ans.Dpf.GrpcClient.dll", "DPFClientAPI.dll"])
+    if "any" == requested_platform:
+        binaries_to_move.extend(["_version.py"])
+
+    for binary_name in binaries_to_move:
+        src = os.path.join(gatebin_folder_path, binary_name)
+        dst = os.path.join(tmpdirname, binary_name)
+        print(f"Moving {src} to {dst}")
+        shutil.move(src=src, dst=dst)
+        moved.append([dst, src])
+
+    if "any" == requested_platform:
+        # Also remove the gatebin folder
+        os.rmdir(gatebin_folder_path)
+
+    # Call the build
+    if not args.wheelhouse:
+        cmd = [sys.executable, "-m", "build", "--wheel"]
+    else:
+        cmd = [sys.executable, "-m", "pip", "wheel", "-w", "dist", "."]
+    try:
+        subprocess.run(cmd, capture_output=False, text=True)
+        print("Done building the wheel.")
+    except Exception as e:
+        print(f"Build failed with error: {e}")
+
+    if "any" == requested_platform:
+        # Recreate the gatebin folder
+        os.mkdir(gatebin_folder_path)
+
+    # Move binaries back
+    for move_back in moved:
+        print(f"Moving back {move_back[0]} to {move_back[1]}")
+        shutil.move(src=move_back[0], dst=move_back[1])
+    print("Binaries moved back.")
+
+    print(f"Done building {requested_platform} wheel for ansys-dpf-core!")
