e35f9aa232662fb80e3bffe4d30d0d57f1cb3283
paul.profizi <paul.profizi@ansys.com>
2023-10-13 17:49:44 +0200

    Add debug prints to create_sites_for_python_operators.ps1

--- docs/source/user_guide/create_sites_for_python_operators.ps1
+++ docs/source/user_guide/create_sites_for_python_operators.ps1
@@ -21,11 +21,16 @@ if (Test-Path ($tempfolder + "/venv/Lib/site-packages/__pycache__")){
     Remove-Item -Recurse -Force ($tempfolder + "/venv/Lib/site-packages/__pycache__")
 }
 $dirName=[System.IO.Path]::GetDirectoryName($zippath)
+Write-Host "Create directory"
 echo $dirName
 if (-Not (Test-Path $dirName)){
     New-Item $dirName -Type directory
 }
+Write-Host "Compress archive"
 Compress-Archive ($tempfolder+"/venv/Lib/site-packages/*") -DestinationPath $zippath -Force
 
+Write-Host "Deactivate venv"
 deactivate
+Write-Host "Remove venv"
 Remove-Item -Recurse -Force ($tempfolder + "/venv")
+Write-Host "Done creating site-packages for gltf plugin"
