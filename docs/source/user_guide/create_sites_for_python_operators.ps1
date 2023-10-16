a54332f1cf080c5d292344387e01cb551c7ff105
paul.profizi <paul.profizi@ansys.com>
2023-10-16 13:24:39 +0200

    Fix create_sites_for_python_operators.ps1

--- docs/source/user_guide/create_sites_for_python_operators.ps1
+++ docs/source/user_guide/create_sites_for_python_operators.ps1
@@ -6,8 +6,10 @@ Write-Host "-pythonexe" $pythonexe
 Write-Host "-tempfolder" $tempfolder
 
 # Expand shortcut in path
+Write-Host "initial tempfolder" $tempfolder
 $tempfolder = Resolve-Path -Path $tempfolder
 $tempfolder = $tempfolder.ToString()
+Write-Host "tempfolder" $tempfolder
 
 Write-Host "make venv"
 Start-Process $pythonexe -ArgumentList ("-m venv "+ $tempfolder+"\venv") -NoNewWindow -Wait
