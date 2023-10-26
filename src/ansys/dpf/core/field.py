2ab72dae22f2cb0b89fe6b1ded0f08d8b40fddad
Antoine Karcher <antoine.karcher@ansys.com>
2023-10-26 17:48:37 +0200

    fix condition for inprocess discrimination

--- src/ansys/dpf/core/field.py
+++ src/ansys/dpf/core/field.py
@@ -240,7 +240,8 @@ class Field(_FieldBase):
                [0.3, 0.6, 0.9]]), [[0.30000000000000004, 0.6000000000000001, 0.8999999999999999], [0.30000000000000004, 0.6000000000000001, 0.8999999999999999]])
 
         """  # noqa: E501
-        if self._server is not None:
+        # Do not copy data if using InProcess server
+        if self._server.client is not None: 
             return _LocalField(self)
         else:
             return self
