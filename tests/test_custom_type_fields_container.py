17340f6b64f7087f4abf9693219397202fb99e03
paul.profizi <paul.profizi@ansys.com>
2023-11-06 09:19:33 +0100

    Add test_custom_type_fields_container.py

---
+++ tests/test_custom_type_fields_container.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+import conftest
+
+import ansys.dpf.core as dpf
+
+
+def integer_field(server):
+    field = dpf.CustomTypeField(np.uint64, server=server)
+    list_ids = [1, 2, 4, 6, 7]
+    scop = dpf.Scoping(ids=list_ids, location=dpf.locations.nodal, server=server)
+    field.scoping = scop
+    list_data = np.array([20, 30, 50, 70, 80], dtype=np.uint64)
+    field.data = list_data
+    return field
+
+
+@conftest.raises_for_servers_version_under("5.0")
+def test_add_field_custom_type_fields_container(server_type):
+    field = integer_field(server_type)
+    fc = dpf.CustomTypeFieldsContainer(np.uint64, server_type)
+    fc.add_label("time", 1)
+    fc.add_field_by_time_id(field)
+    print(fc)
