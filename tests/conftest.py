df3e45d65fb836430cd54f9afded02af7f7af80c
MichaelNale <122869149+MichaelNale@users.noreply.github.com>
2023-11-22 17:50:23 +0100

    Modify CFF tests to be consistent with CFX zone refactoring (#1286)
    * Modify CFF tests to be consistent with CFX zone refactoring
    
    * Update the PR : don't break retro compatibility

--- tests/conftest.py
+++ tests/conftest.py
@@ -290,6 +290,9 @@ def cfx_mixing_elbow():
     return return_ds
 
 
+SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_8_0 = meets_version(
+    get_server_version(core._global_server()), "8.0"
+)
 SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_7_1 = meets_version(
     get_server_version(core._global_server()), "7.1"
 )
