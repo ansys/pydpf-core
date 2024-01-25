df35b8c80f383828855b5cce3a87fc942cdeb97f
Antoine Karcher <antoine.karcher@ansys.com>
2024-01-25 17:44:00 +0100

    attempt to fix the test

--- tests/entry/test_entry.py
+++ tests/entry/test_entry.py
@@ -21,8 +21,6 @@ def test_license_agr(restore_accept_la_env):
     init_val = os.environ["ANSYS_DPF_ACCEPT_LA"]
     del os.environ["ANSYS_DPF_ACCEPT_LA"]
     with pytest.raises(errors.DPFServerException):
-        dpf.start_local_server(config=config, as_global=True)
-    with pytest.raises(errors.DPFServerException):
         dpf.Operator("stream_provider").run()
     os.environ["ANSYS_DPF_ACCEPT_LA"] = init_val
     dpf.start_local_server(config=config, as_global=True)
