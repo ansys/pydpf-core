c6c9f9620cac2bb4b988063111e68241b08178fa
cbellot <camille.bellot@ansys.com>
2024-02-06 12:03:56 +0100

    retro

--- tests/test_generic_data_container.py
+++ tests/test_generic_data_container.py
@@ -1,6 +1,6 @@
 from ansys.dpf import core as dpf
 from conftest import (
-    SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_7_0,
+    SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_7_0, SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_8_0,
 )
 import pytest
 
@@ -101,22 +101,37 @@ def test_get_by_type_generic_data_container(server_type):
     assert "hello world" == new_entity
     new_entity = gdc.get_property("my-string", dpf.types.string)
     assert "hello world" == new_entity
-    new_entity = gdc.get_property("my-string", bytes)
-    assert b"hello world" == new_entity
-    new_entity = gdc.get_property("my-string", dpf.types.bytes)
-    assert b"hello world" == new_entity
 
     entity = dpf.Field(location="phase", nature=dpf.natures.scalar, server=server_type)
     gdc.set_property("my-field", entity)
     new_entity = gdc.get_property("my-field")
-    assert isinstance(new_entity,  dpf.Field)
+    assert isinstance(new_entity, dpf.Field)
 
     new_entity = gdc.get_property("my-field", dpf.Field)
-    assert isinstance(new_entity,  dpf.Field)
+    assert isinstance(new_entity, dpf.Field)
 
     new_entity = gdc.get_property("my-field", dpf.types.field)
-    assert isinstance(new_entity,  dpf.Field)
+    assert isinstance(new_entity, dpf.Field)
 
     with pytest.raises(Exception):
         gdc.get_property("my-field", int)
 
+
+@pytest.mark.skipif(
+    not SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_8_0, reason="Available for servers >=8.0"
+)
+def test_get_bytes_generic_data_container(server_type):
+    gdc = dpf.GenericDataContainer(server=server_type)
+
+    entity = "hello world"
+    gdc.set_property("my-string", entity)
+    new_entity = gdc.get_property("my-string")
+    assert "hello world" == new_entity
+    new_entity = gdc.get_property("my-string", str)
+    assert "hello world" == new_entity
+    new_entity = gdc.get_property("my-string", dpf.types.string)
+    assert "hello world" == new_entity
+    new_entity = gdc.get_property("my-string", bytes)
+    assert b"hello world" == new_entity
+    new_entity = gdc.get_property("my-string", dpf.types.bytes)
+    assert b"hello world" == new_entity
