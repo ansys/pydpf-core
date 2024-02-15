9c470ab9a211a1e5aca0ea9962c90a8e4b3b38f5
paul.profizi <paul.profizi@ansys.com>
2024-02-15 10:53:57 +0100

    Try skip problematic doctest lines
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>

--- src/ansys/dpf/core/server_factory.py
+++ src/ansys/dpf/core/server_factory.py
@@ -235,9 +235,9 @@ class ServerConfig:
     ...     protocol=dpf.server_factory.CommunicationProtocols.gRPC, legacy=False)
     >>> legacy_grpc_config = dpf.ServerConfig(
     ...     protocol=dpf.server_factory.CommunicationProtocols.gRPC, legacy=True)
-    >>> in_process_server = dpf.start_local_server(config=in_process_config, as_global=False)
-    >>> grpc_server = dpf.start_local_server(config=grpc_config, as_global=False)
-    >>> legacy_grpc_server = dpf.start_local_server(config=legacy_grpc_config, as_global=False)
+    >>> in_process_server = dpf.start_local_server(config=in_process_config, as_global=False)  # doctest: +SKIP
+    >>> grpc_server = dpf.start_local_server(config=grpc_config, as_global=False)  # doctest: +SKIP
+    >>> legacy_grpc_server = dpf.start_local_server(config=legacy_grpc_config, as_global=False)  # doctest: +SKIP
 
     Use the environment variable to set the default server configuration.
 
@@ -358,6 +358,9 @@ class AvailableServerConfigs:
     >>> in_process_config = dpf.AvailableServerConfigs.InProcessServer
     >>> grpc_config = dpf.AvailableServerConfigs.GrpcServer
     >>> legacy_grpc_config = dpf.AvailableServerConfigs.LegacyGrpcServer
+    >>> in_process_server = dpf.start_local_server(config=in_process_config, as_global=False)  # doctest: +SKIP
+    >>> grpc_server = dpf.start_local_server(config=grpc_config, as_global=False)  # doctest: +SKIP
+    >>> legacy_grpc_server = dpf.start_local_server(config=legacy_grpc_config, as_global=False)  # doctest: +SKIP
 
     """
 
