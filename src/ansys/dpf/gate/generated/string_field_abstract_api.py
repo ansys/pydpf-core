977ad83798e0491ff32cfef1bb28d680a7921630
rlagha <rlagha@users.noreply.github.com>
2024-04-01 12:16:48 +0000

    update generated code

--- src/ansys/dpf/gate/generated/string_field_abstract_api.py
+++ src/ansys/dpf/gate/generated/string_field_abstract_api.py
@@ -40,6 +40,14 @@ class StringFieldAbstractAPI:
 		raise NotImplementedError
 
 	@staticmethod
+	def string_field_get_entity_data_for_dpf_vector(dpf_object, out, data, size, EntityIndex):
+		raise NotImplementedError
+
+	@staticmethod
+	def string_field_get_entity_data_by_id_for_dpf_vector(dpf_object, vec, data, size, EntityId):
+		raise NotImplementedError
+
+	@staticmethod
 	def csstring_field_get_cscoping(field):
 		raise NotImplementedError
 
@@ -60,11 +68,19 @@ class StringFieldAbstractAPI:
 		raise NotImplementedError
 
 	@staticmethod
+	def string_field_push_back(field, EntityId, size, data):
+		raise NotImplementedError
+
+	@staticmethod
 	def csstring_field_resize(field, dataSize, scopingSize):
 		raise NotImplementedError
 
 	@staticmethod
 	def csstring_field_reserve(field, dataSize, scopingSize):
+		raise NotImplementedError
+
+	@staticmethod
+	def string_field_fast_access_ptr(field):
 		raise NotImplementedError
 
 	@staticmethod
