5aaac3eae947fa17cb27cb24f6889793770c966c
Divya Gupta <divya.gupta@ansys.com>
2024-04-10 18:15:07 +0530

    updated 242 revisioning

--- src/ansys/dpf/core/_version.py
+++ src/ansys/dpf/core/_version.py
@@ -1,6 +1,6 @@
 """Version for ansys-dpf-core"""
 # major, minor, patch
-version_info = 0, 11, 1, "dev0"
+version_info = 0, 12, 0, "dev0"
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
@@ -23,6 +23,7 @@ class ServerToAnsysVersion:
         "7.1": "2024R1",
         "8.0": "2024R2",
         "8.1": "2024R2",
+        "9.0": "2025R1",
     }
 
     def __getitem__(self, item):
