4f6680d66462425e881d0a242c9e461d222d9b4b
rlagha <rlagha@users.noreply.github.com>
2024-04-11 09:21:18 +0000

    update generated code

--- src/ansys/dpf/core/operators/utility/__init__.py
+++ src/ansys/dpf/core/operators/utility/__init__.py
@@ -55,6 +55,7 @@ from .merge_to_field_matrix import merge_to_field_matrix
 from .merge_weighted_fields import merge_weighted_fields 
 from .merge_weighted_fields_containers import merge_weighted_fields_containers 
 from .mesh import mesh 
+from .mesh_to_mc import mesh_to_mc 
 from .overlap_fields import overlap_fields 
 from .producer_consumer_for_each import producer_consumer_for_each 
 from .property_field import property_field 
