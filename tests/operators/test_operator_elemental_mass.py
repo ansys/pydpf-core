ebd4ce79781c51049c433f225484484c724400d9
paul.profizi <paul.profizi@ansys.com>
2024-04-29 15:35:16 +0200

    Fix retro-compatibility
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>

--- tests/operators/test_operator_elemental_mass.py
+++ tests/operators/test_operator_elemental_mass.py
@@ -1,6 +1,12 @@
+import conftest
+import pytest
 import ansys.dpf.core as dpf
 
 
+@pytest.mark.skipif(
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_9_0,
+    reason="Elemental mass operator not functional before 9.0,",
+)
 def test_operator_elemental_mass_rst(allkindofcomplexity):
     model = dpf.Model(allkindofcomplexity)
     op = dpf.operators.result.elemental_mass(data_sources=model)
