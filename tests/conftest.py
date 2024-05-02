a103426e655b5b83f890206ef594d2b92e99eeb4
paul.profizi <paul.profizi@ansys.com>
2024-05-02 11:33:59 +0200

    Revert "Kill servers between each test"
    This reverts commit 2b481ea7d2a110d25e6b7045b32810b6303b502a.

--- tests/conftest.py
+++ tests/conftest.py
@@ -576,7 +576,7 @@ def local_server():
     return local_servers[0]
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=False)
 def count_servers(request):
     """Count servers once we are finished."""
 
