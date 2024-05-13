e073ebf240ed4819324f80ad970be741a4d6a114
rlagha <74787757+rlagha@users.noreply.github.com>
2024-05-13 18:43:04 +0200

    Launch server by version (#1557)
    * Add a helper method which allows to generate new functions per installed DPF server to ease choosing which server to launch
    
    * Add a helper method which allows to generate new functions per installed DPF server to ease choosing which server to launch
    
    * deactivate the test on docker
    
    * fixes according to feedbacks
    
    * Could not connect to DPF legacy gRPC server with version 1.0.
    
    * Revert "Could not connect to DPF legacy gRPC server with version 1.0."
    
    This reverts commit 93c87f0c479156980d48e09c45eeffbb45cd75b9.

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,6 +6,7 @@ import platform
 import psutil
 import sys
 import os
+import packaging.version
 
 from ansys import dpf
 from ansys.dpf.core import errors, server_types
@@ -14,6 +15,7 @@ from ansys.dpf.core.server import set_server_configuration, _global_server
 from ansys.dpf.core.server import start_local_server, connect_to_server
 from ansys.dpf.core.server import shutdown_all_session_servers, has_local_server
 from ansys.dpf.core.server import get_or_create_server
+from ansys.dpf.core import server
 from conftest import (
     SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_4_0,
     running_docker,
@@ -225,17 +227,17 @@ def test_shutting_down_when_deleted():
 
 def test_eq_server_config():
     assert (
-        dpf.core.AvailableServerConfigs.InProcessServer
-        == dpf.core.AvailableServerConfigs.InProcessServer
+            dpf.core.AvailableServerConfigs.InProcessServer
+            == dpf.core.AvailableServerConfigs.InProcessServer
     )
     assert dpf.core.AvailableServerConfigs.GrpcServer == dpf.core.AvailableServerConfigs.GrpcServer
     assert (
-        dpf.core.AvailableServerConfigs.LegacyGrpcServer
-        == dpf.core.AvailableServerConfigs.LegacyGrpcServer
+            dpf.core.AvailableServerConfigs.LegacyGrpcServer
+            == dpf.core.AvailableServerConfigs.LegacyGrpcServer
     )
     assert (
         not dpf.core.AvailableServerConfigs.LegacyGrpcServer
-        == dpf.core.AvailableServerConfigs.InProcessServer
+            == dpf.core.AvailableServerConfigs.InProcessServer
     )
     assert dpf.core.AvailableServerConfigs.LegacyGrpcServer == dpf.core.ServerConfig(
         protocol=dpf.core.server_factory.CommunicationProtocols.gRPC, legacy=True
@@ -316,6 +318,25 @@ def test_check_ansys_grpc_dpf_version_raise():
 
     print(MockServer(remote_server).version)
     with pytest.raises(
-        ValueError, match="Error connecting to DPF LegacyGrpcServer with version 1.0"
+            ValueError, match="Error connecting to DPF LegacyGrpcServer with version 1.0"
     ):
         dpf.core.server_types.check_ansys_grpc_dpf_version(MockServer(remote_server), timeout=2.0)
+
+
+@pytest.mark.skipif(
+    running_docker,
+    reason="can only work with local DPF server install",
+)
+def test_available_servers():
+    out = server.available_servers()
+    assert len(out) > 0
+    for version in out:
+        vout = str(version).replace(".", "_")
+        if packaging.version.parse(version) > packaging.version.parse("2022.1"):
+            meth = "start_" + vout + "_server"
+            assert (hasattr(server, meth))
+            start = getattr(server, meth)
+            srv = start()
+            assert srv.local_server
+            srv = out[version]()
+            assert srv.local_server
