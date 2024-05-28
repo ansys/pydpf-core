97588b62f278b2c2a8d96eab56e971ba39565aeb
Paul Profizi <100710998+PProfizi@users.noreply.github.com>
2024-05-28 14:36:44 +0200

    Fix environment PATH pollution by CGrpc servers (#1590)
    * Extract logic to get system's actual path
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>
    
    * Try commenting all path manipulations for CGrpc server in ServerFactory.get_server_type_from_config
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>
    
    * Add test on path pollution
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>
    
    * Make test retro-compatible (no server.release()
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>
    
    * Try and make get_system_path work on Linux
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>
    
    * Fix style in test
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>
    
    * Confirmed the old hack is no longer required
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>
    
    ---------
    
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>

--- src/ansys/dpf/core/server_types.py
+++ src/ansys/dpf/core/server_types.py
@@ -9,6 +9,7 @@ import io
 import os
 import socket
 import subprocess
+import sys
 import time
 import warnings
 import traceback
@@ -927,14 +928,7 @@ class InProcessServer(CServer):
             # Forced to use ctypes to get the updated PATH due to sys.exec not the Python
             # interpreter when running Python plugin test VS project
             # The better solution would be to not need to update the path
-            windll.kernel32.GetEnvironmentVariableA.argtypes = (c_char_p, c_char_p, c_int)
-            windll.kernel32.GetEnvironmentVariableA.restype = c_int
-            name = "PATH"
-            b_name = name.encode("utf-8")
-            size = 32767
-            buffer = create_string_buffer(b"", size)
-            _ = windll.kernel32.GetEnvironmentVariableA(b_name, buffer, size)
-            os.environ["PATH"] = buffer.value.decode("utf-8")
+            os.environ["PATH"] = get_system_path()
 
     @property
     def version(self):
@@ -975,6 +969,22 @@ class InProcessServer(CServer):
     @property
     def config(self):
         return server_factory.AvailableServerConfigs.InProcessServer
+
+
+def get_system_path() -> str:
+    """Return the current PATH environment variable value of the system."""
+    if not os.name == "posix":
+        windll.kernel32.GetEnvironmentVariableA.argtypes = (c_char_p, c_char_p, c_int)
+        windll.kernel32.GetEnvironmentVariableA.restype = c_int
+        name = "PATH"
+        b_name = name.encode("utf-8")
+        size = 32767
+        buffer = create_string_buffer(b"", size)
+        _ = windll.kernel32.GetEnvironmentVariableA(b_name, buffer, size)
+        return buffer.value.decode("utf-8")
+    else:
+        return sys.path
+
 
 
 class LegacyGrpcServer(BaseServer):
