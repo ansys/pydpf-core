dcf67f75f24e50d1518e3bafa298ae3b36504479
paul.profizi <paul.profizi@ansys.com>
2024-06-14 13:41:48 +0200

    Skip test <232
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>

--- tests/test_service.py
+++ tests/test_service.py
@@ -481,6 +481,7 @@ def test_context_environment_variable(reset_context_environment_variable):
             continue
 
 
+@pytest.mark.skipif(not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_6_0)
 def test_server_without_context(remote_config_server_type):
     """Tests starting a server without a no_context given."""
     server = dpf.core.start_local_server(
