e0df1301ba19b34a3ffb8e0bbc032672e5e3bd55
paul.profizi <paul.profizi@ansys.com>
2024-06-19 16:15:57 +0200

    Add an example for CFX .res files
    Signed-off-by: paul.profizi <paul.profizi@ansys.com>

---
+++ examples/15-cfx/00-cfx_res_files.py
@@ -0,0 +1,29 @@
+"""
+.. _ref_cfx_res_files:
+
+Read CFX `.res` files
+---------------------
+
+This example demonstrates how to read Ansys CFX `.res` files.
+
+.. note::
+    This example requires DPF 7.0 (ansys-dpf-server-2024-1-pre0) or above.
+    For more information, see :ref:`ref_compatibility`.
+
+"""
+
+###############################################################################
+# Exploring an Ansys CFX `.res` file
+# ----------------------------------
+# The first part of the example demonstrates how you can load an
+# Ansys CFX `.res` file in a model.
+
+import ansys.dpf.core as dpf
+from ansys.dpf.core import examples
+
+path = examples.download_cfx_mixing_elbow()
+ds = dpf.DataSources()
+ds.set_result_file_path(path, key="cas")
+ds.add_file_path(path, key="dat")
+model = dpf.Model(data_sources=ds)
+print(model)
