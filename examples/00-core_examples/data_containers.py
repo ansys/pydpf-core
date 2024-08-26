36a2678a87ad62a735342fcda7ac1905dbf37b2d
luisaFelixSalles <luisa.felixsalles@ansys.com>
2024-08-26 11:11:41 +0200

    start of a  new data containers example

---
+++ examples/00-core_examples/data_containers.py
@@ -0,0 +1,20 @@
+from ansys.dpf import core as dpf
+
+"""
+.. _ref_data_containers:
+
+Data Containers
+===============
+When DPF uses operators to load and operate on data, it uses field containers and fields to store and return data. 
+During a numerical simulation, the result data is defined by values associated to entities (scoping).
+
+
+The GenericDataContainer operator is a class available as a submodule of the 'ansys.dpf.core' package. It maps 
+properties to their DPF supported Data Types if they are not previously specified
+
+1) function set_property()
+--------------------------
+If you ae 
+
+
+"""
