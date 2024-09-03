841b0341569971973e5008d24817ad89772a2e9e
Antoine Karcher <antoine.karcher@ansys.com>
2024-09-03 14:08:42 +0200

    add test

--- tests/test_streams_container.py
+++ tests/test_streams_container.py
@@ -45,3 +45,25 @@ def test_create_from_streams_container(server_in_process, simple_bar):
     streams_provider = model.metadata.streams_provider
     sc = streams_provider.outputs.streams_container()
     dpf.core.streams_container.StreamsContainer(streams_container=sc, server=server_in_process)
+
+def test_retrieve_ip(server_in_process):
+    start_server = dpf.core.Operator("grpc::stream_provider", server=server_in_process)
+    in_thread = 1
+    should_start_server = 3
+
+    start_server.connect(2, in_thread)
+    start_server.connect(3, should_start_server)
+
+    streams = start_server.get_output(0, dpf.core.types.streams_container)
+    ds = streams.datasources()
+
+    # look for ip
+    assert len(ds.result_files) == 1
+    assert ds.result_key == "grpc"
+
+    addr = ds.result_files[0]
+    import re
+    # can match 999.999.999.999:99999, 0.0.0.0:0
+    # but not 0.0.0:0, 9999.999.999.999:999, 0.0.0.0
+    ip_addr_regex = r"([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5}"
+    assert re.match(ip_addr_regex, addr) != None 
