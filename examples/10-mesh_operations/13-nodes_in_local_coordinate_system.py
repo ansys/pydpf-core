56dc2a476e20dbe8eb142a29228fd1bba1dc6be6
oparreno <oscar.moroparreno@ansys.com>
2024-09-10 16:23:01 +0200

    fix example

--- examples/10-mesh_operations/13-nodes_in_local_coordinate_system.py
+++ examples/10-mesh_operations/13-nodes_in_local_coordinate_system.py
@@ -33,7 +33,7 @@ ncoord_f = model.metadata.meshed_region.nodes.coordinates_field
 ###############################################################################
 # Get the rotation matrix of the LCS ID 12.
 # The first 9 values in the ``cs`` output is the rotation matrix.
-cs = model.operator(r"mapdl::rst::CS")
+cs = dpf.operators.result.coordinate_system()
 cs.inputs.cs_id.connect(12)
 cs_rot_mat = cs.outputs.field.get_data().data.T[0:9]
 
