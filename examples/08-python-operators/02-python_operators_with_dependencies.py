869c6f3f5824b37057d5d831f7576988188b3a69
luisaFelixSalles <luisa.felixsalles@ansys.com>
2024-09-23 14:54:29 +0200

    corrections files path

--- examples/08-python-operators/02-python_operators_with_dependencies.py
+++ examples/08-python-operators/02-python_operators_with_dependencies.py
@@ -123,9 +123,9 @@ with open(os.path.join(plugin_path, "requirements.txt"), "r") as f:
 # Download the script for your operating system.
 #
 # - For Windows, download this
-#   :download:`PowerShell script </user_guide/create_sites_for_python_operators.ps1>`.
+#   :download:`PowerShell script </user_guide/how-to/create_sites_for_python_operators.ps1>`.
 # - For Linux, download this
-#   :download:`Shell script </user_guide/create_sites_for_python_operators.sh>`.
+#   :download:`Shell script </user_guide/how-to/create_sites_for_python_operators.sh>`.
 #
 # Run the downloaded script with the mandatory arguments:
 #
@@ -157,6 +157,7 @@ if os.name == "nt" and not os.path.exists(
         "doc",
         "source",
         "user_guide",
+        "how-to",
         "create_sites_for_python_operators.ps1",
     )
     args = [
@@ -189,6 +190,7 @@ elif os.name == "posix" and not os.path.exists(
         "doc",
         "source",
         "user_guide",
+        "how-to",
         "create_sites_for_python_operators.sh",
     )
     run_cmd = f"{cmd_file}"
