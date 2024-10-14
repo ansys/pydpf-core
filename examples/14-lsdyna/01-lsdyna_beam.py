d860a33b2adc5cc1321712e8ed9e8aa8a15150c1
luisaFelixSalles <luisa.felixsalles@ansys.com>
2024-10-14 16:42:22 +0200

    new ls dyna example

--- examples/14-lsdyna/01-lsdyna_beam.py
+++ examples/14-lsdyna/01-lsdyna_beam.py
@@ -54,7 +54,7 @@ ds = dpf.DataSources()
 ds.set_result_file_path(d3plot[0], "d3plot")
 ds.add_file_path(d3plot[3], "actunits")
 my_model = dpf.Model(ds)
-# print(model)
+# print(my_model)
 
 ###############################################################################
 # The model has solid (3D) elements and beam (1D) elements. Some of the results
@@ -64,55 +64,97 @@ my_model = dpf.Model(ds)
 # By splitting the mesh by element shape we see that the ball is made by the solid
 # 3D elements and the plate by the beam 1D elements
 
+# Define the analysis mesh
 my_meshed_region = my_model.metadata.meshed_region
 
+# Get separate meshes for each body
 my_meshes = ops.mesh.split_mesh(
     mesh=my_meshed_region, property=dpf.common.elemental_properties.element_shape
 ).eval()
-my_ball_mesh = my_meshes.get_mesh(label_space_or_index={"body": 1, "elshape": 1})
-my_plate_mesh = my_meshes.get_mesh(label_space_or_index={"body": 2, "elshape": 2})
+
+# Define the meshes in separate variables
+ball_mesh = my_meshes.get_mesh(label_space_or_index={"body": 1, "elshape": 1})
+plate_mesh = my_meshes.get_mesh(label_space_or_index={"body": 2, "elshape": 2})
+
 # print(my_meshes)
 ###############################################################################
 # Ball
 
-# print(my_meshes[0])
-# my_meshes[0].plot()
+# print(ball_mesh)
+# my_ball_mesh.plot()
 
 ###############################################################################
 # Plate
 
-# print(my_meshes[1])
-# my_meshes[1].plot()
-
+# print(my_plate_mesh)
+# my_plate_mesh.plot()
 
 ###############################################################################
-# We can split the mesh scoping so it's easier to chose from which body we are
-# analysing the results
 
 my_meshes_scopings = ops.scoping.split_on_property_type(mesh=my_meshed_region).eval()
-my_ball_scoping = my_meshes_scopings.get_scoping(label_space_or_index={"elshape": 1})
-my_plate_scoping = my_meshes_scopings.get_scoping(label_space_or_index={"elshape": 2})
 
-my_time_scoping = my_model.metadata.time_freq_support.time_frequencies
-# For example the ball velocity
-v = my_model.results.velocity(time_scoping=my_time_scoping).eval()
+# Define the mesh scoping in separate variables
+# Here we have a elemental location
+ball_scoping = my_meshes_scopings.get_scoping(label_space_or_index={"elshape": 1})
+plate_scoping = my_meshes_scopings.get_scoping(label_space_or_index={"elshape": 2})
 
-
-# Forces
-
+# We will need a nodal location, so we have to transpose the mesh scoping from elemental to nodal
+ball_scoping_nodal = dpf.operators.scoping.transpose(
+    mesh_scoping=ball_scoping, meshed_region=my_meshed_region
+).eval()
+plate_scoping_nodal = dpf.operators.scoping.transpose(
+    mesh_scoping=plate_scoping, meshed_region=my_meshed_region
+).eval()
 ###############################################################################
-# compare results in different time steps
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Comparing results in different time steps
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 1) Define the time steps
+time_steps_set = [2, 6, 12]
+j = -400
+# 2) Copy the mesh of interest. Here it is the plate mesh that we copy along the X axis
+for i in time_steps_set:
+    # Copy the mesh
+    globals()[f"plate_mesh_{i}"] = plate_mesh.deep_copy()
 
-Sballforces = my_model.results.beam_s_shear_force(mesh_scoping=my_ball_scoping).eval()
-Splateforces = my_model.results.beam_s_shear_force(mesh_scoping=my_plate_scoping).eval()
+    # 3) Get the plot coordinates that will be changed
+    coords_to_update = globals()[f"plate_mesh_{i}"].nodes.coordinates_field
+    # 4) Define the coordinates where the new mesh will be placed
+    overall_field = dpf.fields_factory.create_3d_vector_field(
+        num_entities=1, location=dpf.locations.overall
+    )
+    overall_field.append(data=[j, 0.0, 0.0], scopingid=1)
 
+    # 5) Define the updated coordinates
+    new_coordinates = ops.math.add(fieldA=coords_to_update, fieldB=overall_field).eval()
+    coords_to_update.data = new_coordinates.data
+
+    # 6) Extract the result, here we start by getting the displacement
+    globals()[f"my_displacement_{i}"] = my_model.results.displacement(
+        time_scoping=i, mesh_scoping=plate_scoping_nodal
+    ).eval()[0]
+
+    # Increment the coordinate value for the loop
+    j = j - 400
+###############################################################################
+# Use the :class: `Plotter <ansys.dpf.core.plotter.DpfPlotter>` class to add the plots
+# in the same image
 
 comparison_plot = dpf.plotter.DpfPlotter()
+
 comparison_plot.add_field(
-    field=Sballforces.get_field(label_space_or_index={"time": 12}), meshed_region=my_ball_mesh
+    field=my_displacement_2, meshed_region=plate_mesh_2, deform_by=my_displacement_2
 )
 comparison_plot.add_field(
-    field=Splateforces.get_field(label_space_or_index={"time": 12}), meshed_region=my_plate_mesh
+    field=my_displacement_6, meshed_region=plate_mesh_6, deform_by=my_displacement_6
 )
+comparison_plot.add_field(
+    field=my_displacement_12, meshed_region=plate_mesh_12, deform_by=my_displacement_12
+)
+
 comparison_plot.show_figure()
+
+###############################################################################
+# For example the ball velocity
+# v = my_model.results.velocity(time_scoping=my_time_scoping, mesh_scoping=ball_scoping).eval()
+
+###############################################################################
