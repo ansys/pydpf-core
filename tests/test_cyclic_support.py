62df1e9b468bf3e2c313398cb899791fb69e26d9
oparreno <oscar.moroparreno@ansys.com>
2024-11-07 17:49:24 +0100

    adding to existing test new apis

--- tests/test_cyclic_support.py
+++ tests/test_cyclic_support.py
@@ -94,6 +94,9 @@ def test_cyc_support_from_model(cyclic_lin_rst):
     exp = cyc_support.expand_element_id(1, [0, 1, 2])
     assert np.allclose(exp.ids, [1, 10, 19])
 
+    cs = cyc_support.cs()
+    assert np.allclose(cs.scoping.ids, [12])
+
 
 def test_cyc_support_from_to_operator(cyclic_lin_rst, server_type):
     data_sources = dpf.DataSources(cyclic_lin_rst, server=server_type)
@@ -183,6 +186,17 @@ def test_cyc_support_multistage(cyclic_multistage):
         [1, 3596, 5816, 8036, 10256, 12476],
     )
     assert np.allclose(cyc_support.sectors_set_for_expansion(stage_num=1).ids, list(range(0, 12)))
+
+    high_low_map = my_cyclic_support.high_low_map()
+    assert np.allclose(high_low_map.get_entity_data_by_id(1446), 1447)
+    assert np.allclose(high_low_map.get_entity_data_by_id(2946), 2948)
+    assert np.allclose(high_low_map.get_entity_data_by_id(1452), 1466)
+
+    low_high_map = my_cyclic_support.low_high_map(1)
+    assert np.allclose(low_high_map.get_entity_data_by_id(995), 939)
+    assert np.allclose(low_high_map.get_entity_data_by_id(53), 54)
+    assert np.allclose(low_high_map.get_entity_data_by_id(70), 56)
+
 
 
 def test_delete_cyc_support(cyclic_lin_rst, server_type_legacy_grpc):
