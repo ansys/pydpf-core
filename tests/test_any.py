1639c645ae8ded1df0a2816458bfee0d12e1483f
cbellot <camille.bellot@ansys.com>
2024-11-18 15:24:57 +0100

    retro

--- tests/test_any.py
+++ tests/test_any.py
@@ -122,7 +122,10 @@ def test_cast_scoping_any(server_type):
     assert entity.location == new_entity.location
 
 
-@conftest.raises_for_servers_version_under("9.1")
+@pytest.mark.skipif(
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_8_0,
+    reason="for_each not implemented below 8.0",
+)
 def test_cast_workflow_any(server_type):
     entity = dpf.Workflow(server=server_type)
     any_dpf = dpf.Any.new_from(entity)
