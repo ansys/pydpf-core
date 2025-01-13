4fe75fd25f646e8f68bd04e3da2806ecef93810d
PProfizi <paul.profizi@ansys.com>
2025-01-13 16:57:57 +0100

    Fix/refactor ansys.dpf.core._version.ServerToAnsysVersion

--- src/ansys/dpf/core/_version.py
+++ src/ansys/dpf/core/_version.py
@@ -27,31 +27,16 @@ min_server_version = "4.0"
 
 
 class ServerToAnsysVersion:
-    legacy_version_map = {
-        "1.0": "2021R1",
-        "2.0": "2021R2",
-        "3.0": "2022R1",
-        "4.0": "2022R2",
-        "5.0": "2023R1",
-        "6.0": "2023R2",
-        "6.1": "2023R2",
-        "6.2": "2023R2",
-        "7.0": "2024R1",
-        "7.1": "2024R1",
-        "8.0": "2024R2",
-        "8.1": "2024R2",
-        "8.2": "2024R2",
-        "9.0": "2025R1",
-        "9.1": "2025R1",
-        "10.0": "2025R2",
-    }
-
     def __getitem__(self, item):
-        if len(item) == 3:
-            return self.legacy_version_map[item]
-        else:
-            split = item.split(".")
-            return split[0] + "R" + split[1]
+        split = item.split(".")
+        major = int(split[0])
+        minor = int(split[1])
+        if major < 2024:
+            # Detect new cases of XX.Y versions (necessarily XX<2024)
+            # Compute release version equivalent
+            minor = 2 - major % 2
+            major = 2020 + major // 2 + major % 2
+        return f"{major}R{minor}"
 
 
 server_to_ansys_version = ServerToAnsysVersion()
