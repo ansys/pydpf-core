4f18158675bbfe149a685da98a5b335afd353b05
Alexandre Bouthéon <137152543+a-bouth@users.noreply.github.com>
2025-01-24 10:22:42 +0100

    feat: exposing quantity types in PyDPF Core (#1965)
    * feat: exposing quantity types in PyDPF Core
    
    * feat: Update after PR Review
    
    * feat: Update after PR Review (2)
    
    * feat: Update after PR Review (3)
    
    * feat: Update after PR Review (4)
    
    * feat: Added Fields Container to `any` cast
    
    * feat: exposing quantity_type in the GRPC API
    
    * feat: using right name for quantity_types
    
    * feat: fixing exposure of quantity types
    
    * feat: Added gRPC methods for Any cast for FieldsContainer
    
    * feat: removed an useless comment
    
    * feat: fixed style check

--- src/ansys/dpf/core/field_definition.py
+++ src/ansys/dpf/core/field_definition.py
@@ -150,6 +150,59 @@ class FieldDefinition:
         self._api.csfield_definition_fill_dimensionality(self, dim, nature, dim.internal_size)
         return Dimensionality(dim.tolist(), natures(int(nature)))
 
+    @property
+    def quantity_types(self):
+        """Getter for Quantity Types.
+
+        Returns
+        -------
+        str
+            All quantity types of the elementary data for this FieldDefinition.
+        """
+        quantity_types = []
+        for i in range(self.num_quantity_types()):
+            qt = self._api.csfield_definition_get_quantity_type(self, i)
+            quantity_types.append(str(qt))
+
+        return quantity_types
+
+    def add_quantity_type(self, quantity_type_to_add):
+        """Add a new Quantity Type.
+
+        Parameters
+        ----------
+        quantity_type_to_add: str
+            Quantity type to add
+        """
+        self._api.csfield_definition_set_quantity_type(self, quantity_type_to_add)
+
+    def num_quantity_types(self):
+        """Return number of available quantity types.
+
+        Returns
+        -------
+        num_quantity_types : int
+            Number of quantity types
+        """
+        num_quantity_types = self._api.csfield_definition_get_num_available_quantity_types(self)
+        return num_quantity_types
+
+    def is_of_quantity_type(self, quantity_type):
+        """Check if the field definition is of a given quantity type.
+
+        Parameters
+        ----------
+        quantity_type: str
+            Quantity type to check
+
+        Returns
+        -------
+        is_of_quantity_type : bool
+            True if the field definition is of the given quantity type
+        """
+        is_of_quantity_type = self._api.csfield_definition_is_of_quantity_type(self, quantity_type)
+        return is_of_quantity_type
+
     @unit.setter
     def unit(self, value):
         self._api.csfield_definition_set_unit(self, value, None, 0, 0, 0)
--- tests/test_any.py
+++ tests/test_any.py
@@ -144,3 +144,15 @@ def test_cast_operator_any(server_type):
     new_entity = any_dpf.cast()
 
     assert entity.name == new_entity.name
+
+
+@pytest.mark.skipif(
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_10_0,
+    reason="any does not support operator below 10.0",
+)
+def test_cast_fields_container_any(server_type):
+    entity = dpf.FieldsContainer(server=server_type)
+    any_dpf = dpf.Any.new_from(entity)
+    new_entity = any_dpf.cast()
+
+    assert entity.name == new_entity.name
