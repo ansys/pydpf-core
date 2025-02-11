78147eb8ad56491af8c902d4af9ab396f73be680
Marshall Hanmer <marshall.hanmer@ansys.com>
2025-02-11 15:33:36 -0500

    Suggestions

--- .ci/generate_operators_doc.py
+++ .ci/generate_operators_doc.py
@@ -5,19 +5,21 @@ import argparse
 from jinja2 import Template
 from pathlib import Path
 
-def initialize_server(ansys_path=None):
+def initialize_server(ansys_path=None, include_composites=False, include_sound=False):
     server = dpf.start_local_server(ansys_path=ansys_path)
     print(f"Ansys Path: {server.ansys_path}")
     print(f"Server Info: {server.info}")
     print(f"Server Context: {server.context}")
     print(f"Server Config: {server.config}")
     print(f"Server version: {dpf.global_server().version}")
-    print("Loading Composites Plugin")
-    load_library(
-        Path(server.ansys_path) / "dpf" / "plugins" / "dpf_composites" / "composite_operators.dll"
-    )
-    print("Loading Acoustics Plugin")
-    load_library(Path(server.ansys_path) / "Acoustics" / "SAS" / "ads" / "dpf_sound.dll")
+    if include_composites:
+        print("Loading Composites Plugin")
+        load_library(
+            Path(server.ansys_path) / "dpf" / "plugins" / "dpf_composites" / "composite_operators.dll"
+        )
+    if include_sound:
+        print("Loading Acoustics Plugin")
+        load_library(Path(server.ansys_path) / "Acoustics" / "SAS" / "ads" / "dpf_sound.dll")
     return server
 
 
@@ -59,30 +61,23 @@ def fetch_doc_info(server, operator_name):
             }
         )
     properties = spec.properties
-    if "plugin" in properties:
-        plugin = properties["plugin"]
+    plugin = properties.pop("plugin", "N/A")
+
+    category = properties.pop("category", None)
+
+    scripting_name = properties.pop("scripting_name", None)
+    if category and scripting_name:
+        full_name = category + "." + scripting_name
     else:
-        plugin = "N/A"
-    try:
-        scripting_name = properties["scripting_name"]
-        full_name = properties["category"] + "." + properties["scripting_name"]
-    except KeyError:
-        scripting_name = None
         full_name = None
-    try:
-        user_name = properties["user_name"]
-    except KeyError:
-        user_name = operator_name
-    try:
-        category = properties["category"]
-        op_friendly_name = category + ": " + user_name
-    except KeyError:
-        category = ""
-        op_friendly_name = user_name
-    try:
-        license = properties["license"]
-    except KeyError:
-        license = "None"
+
+    user_name = properties.pop("user_name", operator_name)
+    
+    op_friendly_name = user_name
+    if category:
+        op_friendly_name = category + ":" + op_friendly_name
+
+    license = properties.pop("license", "None")
     scripting_info = {
         "category": category,
         "plugin": plugin,
@@ -136,6 +131,8 @@ def main():
         "--ansys_path", default=None, help="Path to Ansys DPF Server installation directory"
     )
     parser.add_argument("--include_private", action="store_true", help="Include private operators")
+    parser.add_argument("--include_composites", action="store_true", help="Include composites operators")
+    parser.add_argument("--include_sound", action="store_true", help="Include sound operators")
     args = parser.parse_args()
     desired_plugin = args.plugin
 
