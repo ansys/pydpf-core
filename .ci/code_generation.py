f3fe16cd922b2dcf8f37e12b7d8677fea29daa39
PProfizi <paul.profizi@ansys.com>
2025-03-03 11:58:08 +0100

    Do not remove translator.py during code_generation.py

--- .ci/code_generation.py
+++ .ci/code_generation.py
@@ -21,6 +21,8 @@ for file_path in files:
         continue
     if file_path.name == "operator.mustache":
         continue
+    if file_path.stem == "translator":
+        continue
     try:
         if file_path.is_dir():
             shutil.rmtree(file_path)
