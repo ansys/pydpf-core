e95a12ee374c8e5e056c89b4e32bad4559bf4f80
PProfizi <paul.profizi@ansys.com>
2025-03-04 11:07:05 +0100

    Add aliases to operator signature

--- src/ansys/dpf/core/operators/build.py
+++ src/ansys/dpf/core/operators/build.py
@@ -102,6 +102,7 @@ def build_pin_data(pins, output=False):
             "document_pin_docstring": document_pin_docstring,
             "ellipsis": 0 if specification.ellipsis else -1,
             "has_aliases": len(specification.aliases) > 0,
+            "aliases_list": [dict(("alias", alias)) for alias in specification.aliases],
             "aliases": str(specification.aliases),
         }
 
@@ -129,13 +130,13 @@ def build_operator(
     input_pins = []
     if specification.inputs:
         input_pins = build_pin_data(specification.inputs)
-    has_input_aliases = any(len(pin["aliases"]) > len("[]") for pin in input_pins)
+    has_input_aliases = any(len(pin["aliases_list"]) > 0 for pin in input_pins)
 
     output_pins = []
     if specification.outputs:
         output_pins = build_pin_data(specification.outputs, output=True)
     multiple_output_types = any(pin["multiple_types"] for pin in output_pins)
-    has_output_aliases = any(len(pin["aliases"]) > len("[]") for pin in output_pins)
+    has_output_aliases = any(len(pin["aliases_list"]) > 0 for pin in output_pins)
 
     docstring = build_docstring(specification_description)
 
