b8d425ee0b4822d85435759c023be7a094716900
PProfizi <paul.profizi@ansys.com>
2025-06-02 17:57:02 +0200

    Remove wrong import

--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import pytest
-from trame_common.decorators.klass import change
 
 from ansys.dpf.core.changelog import Changelog
 import conftest
