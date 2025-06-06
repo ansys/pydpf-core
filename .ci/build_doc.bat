d7208a13151c0f0dd3a3414de12c6e8e240b8a05
PProfizi <paul.profizi@ansys.com>
2025-06-06 16:25:12 +0200

    Remove unnecessary storage and copy of plots (initial problem now fixed)

--- .ci/build_doc.bat
+++ .ci/build_doc.bat
@@ -13,16 +13,5 @@ call make html -v -v -v -P
 REM Display the directory contents for verification
 dir
 
-REM Patch pyVista issue with elemental plots by copying necessary images
-xcopy source\examples\04-advanced\02-volume_averaged_stress\sphx_glr_02-volume_averaged_stress_001.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_001.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_002.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_003.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_004.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_005.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_006.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_007.png build\html\_images\ /y /f
-xcopy source\examples\12-fluids\02-fluids_results\sphx_glr_02-fluids_results_thumb.png build\html\_images\ /y /f
-
 REM Return to the original directory
 popd
