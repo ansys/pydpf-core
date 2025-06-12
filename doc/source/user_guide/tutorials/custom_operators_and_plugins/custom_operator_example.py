c760f4e8fe9712d74dd9966fd234871b4caa1d98
PProfizi <paul.profizi@ansys.com>
2025-06-12 14:15:06 +0200

    Add a changelog to the operator in custom_operator_example.py

--- doc/source/user_guide/tutorials/custom_operators_and_plugins/custom_operator_example.py
+++ doc/source/user_guide/tutorials/custom_operators_and_plugins/custom_operator_example.py
@@ -1,4 +1,5 @@
 from ansys.dpf import core as dpf
+from ansys.dpf.core.changelog import Changelog
 from ansys.dpf.core.custom_operator import CustomOperatorBase, record_operator  # noqa: F401
 from ansys.dpf.core.operator_specification import CustomSpecification, SpecificationProperties, \
     PinSpecification
@@ -12,18 +13,26 @@ class CustomOperator(CustomOperatorBase):
     @property
     def specification(self) -> CustomSpecification:
         spec = CustomSpecification()
-        spec.description = "What the Operator does."
+        spec.description = "What the Operator does. You can use MarkDown and LaTeX in descriptions."
         spec.inputs = {
-            0: PinSpecification("name_of_pin_0", [dpf.Field, dpf.FieldsContainer],
-                                "Describe pin 0."),
+            0: PinSpecification(name="name_of_pin_0", type_names=[dpf.Field, dpf.FieldsContainer],
+                                document="Describe input pin 0."),
         }
         spec.outputs = {
-            0: PinSpecification("name_of_pin_0", [dpf.Field], "Describe pin 0."),
+            0: PinSpecification(name="name_of_pin_0", type_names=[dpf.Field], document="Describe output pin 0."),
         }
         spec.properties = SpecificationProperties(
             user_name="user name",
             category="category",
             license="license",
+        )
+        # Set the changelog of the operator to track changes
+        spec.set_changelog(Changelog()
+                           .expect_version("0.0.0")
+                           .patch_bump("Describe a patch bump.")
+                           .major_bump("Describe a major bump.")
+                           .minor_bump("Describe a minor bump.")
+                           .expect_version("1.1.0")
         )
         return spec
 
