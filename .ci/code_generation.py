63d208af1fc172e90869ff8828199ba74a2cb416
PProfizi <paul.profizi@ansys.com>
2025-08-04 16:17:57 +0200

    Add the operator files to non-deleted files during code generation

--- .ci/code_generation.py
+++ .ci/code_generation.py
@@ -11,17 +11,23 @@ from ansys.dpf.core.operators import build
 core.set_default_server_context(core.AvailableServerContexts.premium)
 core.start_local_server(config=core.AvailableServerConfigs.LegacyGrpcServer)
 
+ignore_files = [
+    "operator.mustache",
+    "build.py",
+    "specification.py",
+    "translator.py",
+    # Deprecated operator scripting names
+    "gasket_deformation.py",
+    "gasket_deformation_X.py",
+    "gasket_deformation_XY.py",
+    "gasket_deformation_XZ.py",
+]
+
 local_dir = Path(__file__).parent
 TARGET_PATH = local_dir.parent / "src" / "ansys" / "dpf" / "core" / "operators"
 files = TARGET_PATH.glob("*")
 for file_path in files:
-    if file_path.stem == "specification":
-        continue
-    if file_path.stem == "translator":
-        continue
-    if file_path.name == "build.py":
-        continue
-    if file_path.name == "operator.mustache":
+    if file_path.name in ignore_files:
         continue
     try:
         if file_path.is_dir():
