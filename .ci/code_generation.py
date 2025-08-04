20f2427441a75fea0b5907fc80ed404bd8269820
PProfizi <paul.profizi@ansys.com>
2025-08-04 19:03:49 +0200

    Update code_generation.py to handle kept operators

--- .ci/code_generation.py
+++ .ci/code_generation.py
@@ -11,30 +11,36 @@ from ansys.dpf.core.operators import build
 core.set_default_server_context(core.AvailableServerContexts.premium)
 core.start_local_server(config=core.AvailableServerConfigs.LegacyGrpcServer)
 
-ignore_files = [
-    "operator.mustache",
-    "build.py",
-    "specification.py",
-    "translator.py",
+files_to_keep = {
+    "operator.mustache": "",
+    "build.py": "",
+    "specification.py": "",
+    "translator.py": "",
     # Deprecated operator scripting names
-    "gasket_deformation.py",
-    "gasket_deformation_X.py",
-    "gasket_deformation_XY.py",
-    "gasket_deformation_XZ.py",
-]
+    "result": [
+        "gasket_deformation.py",
+        "gasket_deformation_X.py",
+        "gasket_deformation_XY.py",
+        "gasket_deformation_XZ.py",
+    ],
+}
 
 local_dir = Path(__file__).parent
 TARGET_PATH = local_dir.parent / "src" / "ansys" / "dpf" / "core" / "operators"
 files = TARGET_PATH.glob("*")
 for file_path in files:
-    if file_path.name in ignore_files:
+    if file_path.is_file() and (file_path.name in files_to_keep):
         continue
-    try:
-        if file_path.is_dir():
-            shutil.rmtree(file_path)
+    if file_path.is_dir():
+        shutil.rmtree(file_path / "__pycache__", ignore_errors=True)
+        if file_path.name in files_to_keep:
+            sub_files = file_path.glob("*")
+            for sub_file in sub_files:
+                if sub_file.name not in files_to_keep[file_path.name]:
+                    sub_file.unlink()
         else:
-            file_path.unlink()
-    except:
-        pass
+            shutil.rmtree(file_path)
+    else:
+        file_path.unlink()
 
 build.build_operators()
