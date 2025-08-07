c3f1da6c33301b9e194170457ba0949bc94419c1
Paul Profizi <100710998+PProfizi@users.noreply.github.com>
2025-08-07 11:35:04 +0200

    test(any): fix skip criterion for test_cast_data_sources_any (#2528)

--- tests/test_any.py
+++ tests/test_any.py
@@ -123,8 +123,8 @@ def test_cast_scoping_any(server_type):
 
 
 @pytest.mark.skipif(
-    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_8_0,
-    reason="Requires 24R2.",
+    not conftest.SERVERS_VERSION_GREATER_THAN_OR_EQUAL_TO_11_0,
+    reason="Requires 26R1.",
 )
 def test_cast_data_sources_any(server_type):
     # Not available through grpc yet
