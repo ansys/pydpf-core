7efbb134e87320da3e86b97842cb8f2008faad64
PProfizi <paul.profizi@ansys.com>
2025-08-20 18:29:47 +0200

    Fix Field.deep_copy()

--- src/ansys/dpf/core/field.py
+++ src/ansys/dpf/core/field.py
@@ -943,22 +943,22 @@ class Field(_FieldBase):
             f._data_pointer = self._data_pointer
         except Exception as e:
             raise e
-        if self.meshed_region:
-            try:
+        try:
+            if self.meshed_region:
                 f.meshed_region = self.meshed_region.deep_copy(server=server)
-            except DPFServerException as e:
-                if "the field doesn't have this support type" in str(e):
-                    pass
-                else:
-                    raise e
-        if self.time_freq_support:
-            try:
+        except DPFServerException as e:
+            if "the field doesn't have this support type" in str(e):
+                pass
+            else:
+                raise e
+        try:
+            if self.time_freq_support:
                 f.time_freq_support = self.time_freq_support.deep_copy(server=server)
-            except DPFServerException as e:
-                if "the field doesn't have this support type" in str(e):
-                    pass
-                else:
-                    raise e
+        except DPFServerException as e:
+            if "the field doesn't have this support type" in str(e):
+                pass
+            else:
+                raise e
 
         return f
 
