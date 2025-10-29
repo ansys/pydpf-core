74866e81d1ef431ec528a7a9b54975a5149ddb6f
oparreno <oscar.moroparreno@ansys.com>
2025-10-29 15:02:33 +0100

    testing correction

--- tests/test_resultinfo.py
+++ tests/test_resultinfo.py
@@ -138,7 +138,7 @@ def test_byitem_resultinfo(model):
 
 
 def test_get_result_resultinfo_from_index(model):
-    res = model.metadata.result_info[2]
+    res = model.metadata.result_info[3]
     assert res.name == "acceleration"
     assert res.n_components == 3
     assert res.dimensionality == "vector"
