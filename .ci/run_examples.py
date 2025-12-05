6b438beb3f2660f97be7fdd1b8e7675f3f892015
Antoine Karcher <antoine.karcher@ansys.com>
2025-12-05 15:32:11 +0100

    disable additional example on docker

--- .ci/run_examples.py
+++ .ci/run_examples.py
@@ -24,7 +24,8 @@ skipped_docker = [
     "00-cfx_res_files.py",
     "03-distributed-msup_expansion_steps.py",
     "06-distributed_stress_averaging.py",
-    "01-distributed_workflows_on_remote.py"
+    "01-distributed_workflows_on_remote.py",
+    "00-distributed_total_disp.py"
 ]
 
 for root, subdirectories, files in os.walk(examples_path):
