acb7d20d526ec10aab54f2ca59761b73002fd35e
Paul Profizi <100710998+PProfizi@users.noreply.github.com>
2025-12-16 09:46:36 +0100

    feat(hdf5): assign `.h5` files to `hdf5` key (#2838)
    Co-authored-by: Muhammed Adedigba <68085496+moe-ad@users.noreply.github.com>

--- src/ansys/dpf/core/data_sources.py
+++ src/ansys/dpf/core/data_sources.py
@@ -173,6 +173,8 @@ class DataSources:
         # Look for another extension for .h5 and .cff files
         if key == "" and extension in [".h5", ".cff"]:
             key = self.guess_second_key(str(filepath))
+        if key == "" and extension == ".h5":
+            key = "h5dpf"
         if key == "":
             self._api.data_sources_set_result_file_path_utf8(self, str(filepath))
         else:
