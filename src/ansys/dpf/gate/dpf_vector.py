e31ea1aa726fec2bf38839dfbb867ca0cd1cd92f
Paul Profizi <100710998+PProfizi@users.noreply.github.com>
2026-04-08 17:00:48 +0200

    fix: restore safe exception handling in DPFVector.__del__ methods (#3100)

--- src/ansys/dpf/gate/dpf_vector.py
+++ src/ansys/dpf/gate/dpf_vector.py
@@ -1,7 +1,10 @@
+from contextlib import suppress
 import ctypes
+import sys
 import numpy as np
 
 from ansys.dpf.core.check_version import server_meet_version
+from ansys.dpf.gate.generated import capi as _gate_capi
 from ansys.dpf.gate.generated import dpf_vector_capi
 from ansys.dpf.gate.integral_types import MutableListInt32, MutableInt32, MutableListDouble, \
     MutableListString, MutableListChar
@@ -90,11 +93,11 @@ class DPFVectorBase:
         return self._modified and self.size > 0 # Updating is not necessary for an empty vector. Updating it can cause issue, see #2274
 
     def __del__(self):
+        if sys.is_finalizing() or getattr(_gate_capi, '_api_loading', False):
+            return
         if hasattr(self, "_internal_obj"):
-            try:
+            with suppress(Exception):
                 self.dpf_vector_api.dpf_vector_delete(self)
-            except Exception as e:
-                raise e
 
 
 class DPFVectorInt(DPFVectorBase):
@@ -120,13 +123,12 @@ class DPFVectorInt(DPFVectorBase):
         self.dpf_vector_api.dpf_vector_int_commit(self, self.internal_data, self.internal_size, self.has_changed())
 
     def __del__(self):
-        try:
+        if sys.is_finalizing() or getattr(_gate_capi, '_api_loading', False):
+            return
+        with suppress(Exception):
             if hasattr(self, "_array"):
                 self.dpf_vector_api.dpf_vector_int_free(self, self.internal_data, self.internal_size,
                                                         self.has_changed())
-        except Exception as e:
-            raise e
-
         super().__del__()
 
 
@@ -153,13 +155,12 @@ class DPFVectorDouble(DPFVectorBase):
         self.dpf_vector_api.dpf_vector_double_commit(self, self.internal_data, self.internal_size, self.has_changed())
 
     def __del__(self):
-        try:
+        if sys.is_finalizing() or getattr(_gate_capi, '_api_loading', False):
+            return
+        with suppress(Exception):
             if hasattr(self, "_array"):
                 self.dpf_vector_api.dpf_vector_double_free(self, self.internal_data, self.internal_size,
                                                            self.has_changed())
-        except Exception as e:
-            raise e
-
         super().__del__()
 
 
@@ -219,13 +220,12 @@ class DPFVectorCustomType(DPFVectorBase):
         )
 
     def __del__(self):
-        try:
+        if sys.is_finalizing() or getattr(_gate_capi, '_api_loading', False):
+            return
+        with suppress(Exception):
             if hasattr(self, "_array"):
                 self.dpf_vector_api.dpf_vector_char_free(self, self.internal_data, self.size * self.type.itemsize,
                                                          self.has_changed())
-        except Exception as e:
-            raise e
-
         super().__del__()
 
 
@@ -245,13 +245,12 @@ class DPFVectorString(DPFVectorBase):
         return self._array
 
     def __del__(self):
-        try:
+        if sys.is_finalizing() or getattr(_gate_capi, '_api_loading', False):
+            return
+        with suppress(Exception):
             if self._array:
                 self.dpf_vector_api.dpf_vector_char_ptr_free(self, self.internal_data, self.internal_size,
                                                              self.has_changed())
-        except Exception as e:
-            raise e
-
         super().__del__()
 
     def __len__(self):
