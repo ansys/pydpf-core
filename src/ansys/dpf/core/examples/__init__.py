e2c282b99ea716237755323a7421ade8268b081e
Muhammed Adedigba <muhammed.adedigba@synopsys.com>
2026-05-04 12:52:54 +0200

    fix: codestyle

--- src/ansys/dpf/core/examples/__init__.py
+++ src/ansys/dpf/core/examples/__init__.py
@@ -29,42 +29,42 @@ from .examples import *  # noqa: F403
 def __getattr__(name):
     if name == "simple_bar":
         global simple_bar
-        simple_bar = find_simple_bar() # noqa: F405
+        simple_bar = find_simple_bar()  # noqa: F405
         return simple_bar
     elif name == "static_rst":
         global static_rst
-        static_rst = find_static_rst() # noqa: F405
+        static_rst = find_static_rst()  # noqa: F405
         return static_rst
     elif name == "complex_rst":
         global complex_rst
-        complex_rst = find_complex_rst() # noqa: F405
+        complex_rst = find_complex_rst()  # noqa: F405
         return complex_rst
     elif name == "multishells_rst":
         global multishells_rst
-        multishells_rst = find_multishells_rst() # noqa: F405
+        multishells_rst = find_multishells_rst()  # noqa: F405
         return multishells_rst
     elif name == "electric_therm":
         global electric_therm
-        electric_therm = find_electric_therm() # noqa: F405
+        electric_therm = find_electric_therm()  # noqa: F405
         return electric_therm
     elif name == "steady_therm":
         global steady_therm
-        steady_therm = find_steady_therm() # noqa: F405
+        steady_therm = find_steady_therm()  # noqa: F405
         return steady_therm
     elif name == "transient_therm":
         global transient_therm
-        transient_therm = find_transient_therm() # noqa: F405
+        transient_therm = find_transient_therm()  # noqa: F405
         return transient_therm
     elif name == "msup_transient":
         global msup_transient
-        msup_transient = find_msup_transient() # noqa: F405
+        msup_transient = find_msup_transient()  # noqa: F405
         return msup_transient
     elif name == "simple_cyclic":
         global simple_cyclic
-        simple_cyclic = find_simple_cyclic() # noqa: F405
+        simple_cyclic = find_simple_cyclic()  # noqa: F405
         return simple_cyclic
     elif name == "distributed_msup_folder":
         global distributed_msup_folder
-        distributed_msup_folder = find_distributed_msup_folder() # noqa: F405
+        distributed_msup_folder = find_distributed_msup_folder()  # noqa: F405
         return distributed_msup_folder
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
