d40fc320d81cd3d3f0dad3af42a7b24569300067
Rafael Canton <rcanton@synopsys.com>
2026-06-08 15:14:28 +0200

    Fix MeshedRegion deep_copy for Legacy gRPC

--- src/ansys/dpf/core/meshed_region.py
+++ src/ansys/dpf/core/meshed_region.py
@@ -689,30 +689,9 @@ class MeshedRegion:
         >>> deep_copy = meshed_region.deep_copy(server=other_server)
 
         """
-        if self._server.config.legacy:
-            if self.nodes.scoping is None:  # empty Mesh
-                return MeshedRegion()
-            node_ids = self.nodes.scoping.ids
-            element_ids = self.elements.scoping.ids
-            mesh = MeshedRegion(
-                num_nodes=len(node_ids), num_elements=len(element_ids), server=server
-            )
-            with self.nodes.coordinates_field.as_local_field() as coord:
-                for i, node in enumerate(mesh.nodes.add_nodes(len(node_ids))):
-                    node.id = node_ids[i]
-                    node.coordinates = coord.get_entity_data(i)
-            with self.elements.connectivities_field.as_local_field() as connect:
-                with self.elements.element_types_field.as_local_field() as types:
-                    for i, elem in enumerate(mesh.elements.add_elements(len(element_ids))):
-                        elem.id = element_ids[i]
-                        elem.connectivity = connect.get_entity_data(i)
-                        elem.shape = element_types.shape(types.get_entity_data(i)[0])
-            mesh.unit = self.unit
-            return mesh
-        else:
-            from ansys.dpf.core.core import _deep_copy
+        from ansys.dpf.core.core import _deep_copy
 
-            return _deep_copy(self, server=server)
+        return _deep_copy(self, server=server)
 
     def field_of_properties(self, property_name):
         """
